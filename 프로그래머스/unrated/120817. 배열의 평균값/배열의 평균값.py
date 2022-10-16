90bafb8ef85ff59526f91c3c3b35665d02e33128
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-10-16 23:31:47 +0900

    [unrated] Title: 배열의 평균값, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.py
@@ -0,0 +1,12 @@
+# import math
+
+# def solution(numbers):
+#     answer = sum.(0:)// len((0:]) 
+#     // 어떤 것: 배열의 요소들의 합 / 요소들의 갯수 
+#     // 어떤 것을 리턴 
+#     return answer
+                            
+                            
+def solution(numbers):
+    answer = sum(numbers)/len(numbers)
+    return answer
