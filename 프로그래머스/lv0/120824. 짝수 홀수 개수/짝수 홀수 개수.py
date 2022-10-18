a4f4c2fd59bb13d32982cfdc39adf694fb83db39
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-10-18 14:43:59 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,26 +1,3 @@
-# def solution(num_list):
-#     a = int(num_list)
-#     answer = [len(a % 2 ==0),len(a % 2 !=0)]
-#     return answer
-
-
 def solution(num_list):
-    even = list(filter(lambda x: x % 2==0, num_list))
-    return (len(even), len(num_list)-len(even))
-
-
-
-
-# def solution(num_list):
-#     answer = [0,0]
-#     for n in num_list:
-#         answer[n%2]+=1
-#     return answer
-
-# def solution(num_list):
-#     even = list(filter(lambda x: x%2==0, num_list))
-#     return [len(even), len(num_list)-len(even)]
-
-# def solution(num_list):
-#     even = list(filter(lamda x: x%2==0, num_list))
-#     return [len(even), len(num_list)-len(even)]
+    even = list(filter(lambda x : x % 2 == 0, num_list))
+    return [len(even), len(num_list)-len(even)]
