cf4a8474990e335a085d0f2365086495dedead6a
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-10-24 14:21:12 +0900

    [Bronze IV] Title: 상근날드, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/5543. 상근날드/상근날드.py
+++ 백준/Bronze/5543. 상근날드/상근날드.py
@@ -1,9 +1,3 @@
-burger = []
-for _ in range(3):
-    burger.append(int(input()))
-drink = []
-for _ in range(2):
-    drink.append(int(input()))
-    
-min_set = min(burger) + min(drink) - 50 
-print(min_set)
+burger = [int(input()) for _ in range(3)]
+drink = [int(input()) for _ in range(2)]
+print(min(burger)+min(drink)-50)
