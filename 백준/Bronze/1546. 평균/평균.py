6996b5a42f334173eda02f0080a41b34a28925da
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-10-25 11:18:13 +0900

    [Bronze I] Title: 평균, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -3,8 +3,7 @@ test_list = list(map(int, input().split()))
 max_score = max(test_list)
 
 new_list = []
-for score in test_list:
-    new_list.append(score/max_score *100)
+for score in test_list :
+    new_list.append(score/max_score*100)
 test_avg = sum(new_list)/n
 print(test_avg)
-
