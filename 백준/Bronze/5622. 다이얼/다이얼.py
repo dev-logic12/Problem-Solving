3d05aa0805c962f1982e5ce147f74021af6643af
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-10-29 22:58:59 +0900

    [Bronze II] Title: 다이얼, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,10 +1,10 @@
-list = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+list1 = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
 word = input()
 
 time = 0
-for unit in list :  
-    for i in unit:  # alpabet 리스트에서 각 요소를 꺼내서 한글자씩 분리
-        for x in word :  # 입력받은 문자를 하나씩 분리
-            if i == x :  # 두 알파벳이 같으면
-                time += list.index(unit) +3  # time = time + index +3
+for unit in list1 :
+    for i in unit: 
+        for x in word:
+            if i == x:
+                time += list1.index(unit)+3
 print(time)
