59c6e0ca5a5fab604de557b424655534cd7f7b08
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-10-29 22:24:25 +0900

    [Bronze II] Title: 상수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,8 +1,5 @@
-num1, num2 = input().split()
+num0, num1 = input().split()
+num0 = int(num0[::-1])
 num1 = int(num1[::-1])
-num2 = int(num2[::-1])
 
-if num1 > num2:
-    print(num1)
-else: 
-    print(num2)
+print(num0) if num0>num1 else print(num1)
