6e3a415ff593dccb2aa3bacc13688c08b636c263
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-10-30 10:47:46 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,14 +1,13 @@
-n = int(input())
+N = int(input())
+cnt = N
 
-group_word = 0
-for _ in range(n):
+for i in range(N):
     word = input()
-    error = 0
-    for index in range(len(word)-1):
-        if word[index] != word[index+1]:
-            new_word = word[index+1:]
-            if new_word.count(word[index])>0:
-                error +=1                
-    if error == 0:
-        group_word += 1
-print(group_word)
+    for j in range(0, len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            cnt -= 1
+            break
+
+print(cnt)
