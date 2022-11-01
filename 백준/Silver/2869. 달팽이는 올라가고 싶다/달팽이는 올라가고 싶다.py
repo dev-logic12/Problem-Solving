402bfb1e11d9b5b6190004a58fa82f2b79ee05de
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-11-01 10:54:22 +0900

    [Silver V] Title: 달팽이는 올라가고 싶다, Time: 68 ms, Memory: 32952 KB -BaekjoonHub

--- 백준/Silver/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Silver/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,9 +1,7 @@
-
-
 import math
 
-a, b, v = map(int, input().split())
-# a= 올라가는 길이, b= 떨어지는길이, v= 나무높이 
+a, b ,v = map(int, input().split())
 
-day = math.ceil((v-a)/(a-b)) + 1
-print(day)
+
+d = math.ceil((v-a)/(a-b))+1
+print(d)
