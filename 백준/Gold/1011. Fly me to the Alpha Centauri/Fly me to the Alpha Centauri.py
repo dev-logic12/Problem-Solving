d1c99a3bd11a79ee9e427afff6add9adeeddc61d
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-11-03 09:59:08 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 1176 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -1,14 +1,15 @@
 t = int(input())
 
+
 for _ in range(t):
-    x, y = map(int,input().split())
-    distance = y - x
-    count = 0  # 이동 횟수
-    move = 1  # count별 이동 가능한 거리
-    move_plus = 0  # 이동한 거리의 합
-    while move_plus < distance :
-        count += 1
-        move_plus += move  # count 수에 해당하는 move를 더함
-        if count % 2 == 0 :  # count가 2의 배수일 때, 
-            move += 1  
+    x, y = map(int, input().split())
+    d = y-x 
+    count = 0
+    move = 1
+    move_plus = 0
+    while move_plus < d:
+        count += 1 
+        move_plus += move
+        if count % 2 == 0:
+            move += 1
     print(count)
