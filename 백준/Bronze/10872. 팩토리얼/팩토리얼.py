2ff4b636875a8e77c8150431316f815fe2562911
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-11-05 20:14:50 +0900

    [Bronze V] Title: 팩토리얼, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -1,8 +1,7 @@
-def factorial(n):
-    result = 1
-    if n > 0 :
-        result = n * factorial(n-1)
-    return result
-
 n = int(input())
-print(factorial(n))
+
+result = 1
+if n > 0:
+    for i in range(1, n+1):
+        result *= i
+print(result)
