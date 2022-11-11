9bacc224d2c1e580e97a915007e26225aa898014
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-11-11 09:19:56 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
@@ -0,0 +1,12 @@
+
+def solution(n):
+    answer = 0
+    for i in range(3,n+1) :
+        temp = 1
+        for j in range(1,i) :
+            if i % j == 0 :
+                temp += 1
+
+        if temp > 2 :
+            answer += 1
+    return answer
