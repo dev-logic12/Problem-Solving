99de6b8b08307e260fa2db9a1e1fc340fae6c4ac
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-11-13 15:06:56 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,2 +1,6 @@
 def solution(my_string):
-    return sum(int(i) for i in my_string if i.isdigit())
+    answer = 0
+    for i in my_string:
+        if i.isnumeric():
+            answer += int(i)
+    return answer
