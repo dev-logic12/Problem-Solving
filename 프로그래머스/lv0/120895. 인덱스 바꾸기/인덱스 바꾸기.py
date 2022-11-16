2453356e88c558538fbb282e7455f9d5cb4dbf96
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-11-16 10:39:50 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,3 +1,4 @@
 def solution(my_string, num1, num2):
-    answer = my_string[:num1] + my_string[num2] + my_string[num1+1:num2] + my_string[num1] + my_string[num2+1:]
-    return answer
+    s = list(my_string)
+    s[num1],s[num2] = s[num2],s[num1]
+    return ''.join(s)
