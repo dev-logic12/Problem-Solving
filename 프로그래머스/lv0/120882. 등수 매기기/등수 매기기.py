b92c2b469131eae57e4cfb731bfd7e0b75e336ad
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-11-24 13:37:16 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.py
@@ -1,3 +1,4 @@
 def solution(score):
-    a = sorted([sum(i) for i in score], reverse = True)
-    return [a.index(sum(i))+1 for i in score]
+    평균 = [(i + j)/2 for i, j in score]
+    정렬된점수 = sorted(평균, reverse=True)
+    return [i+1 for i in list(map(정렬된점수.index, 평균))]
