9edaddce18c7219dbb65280425e726606ffa0098
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-11-26 12:28:21 +0900

    [level 0] Title: 문자열 밀기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,14 +1,4 @@
-import collections
 
-def solution(A, B):
-    if A == B:
-        return 0
-    비교 = collections.deque(A)
-    for i in range(len(A)):
-        비교.rotate(1)
-        if ''.join(비교) == B:
-            return i+1
-    return -1
 
 
 solution=lambda a,b:(b*2).find(a)
