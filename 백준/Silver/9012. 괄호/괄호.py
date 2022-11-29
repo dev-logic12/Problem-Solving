444446e2965c0fa43d36fc30935d52c6c1967ce0
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-11-29 11:49:04 +0900

    [Silver IV] Title: 괄호, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,17 +1,5 @@
-a = int(input())
-for i in range(a):
+for _ in range(int(input())):
     b = input()
-    s = list(b)
-    sum = 0
-    for i in s:
-        if i == '(':
-            sum += 1
-        elif i == ')':
-            sum -= 1
-        if sum < 0:
-            print('NO')
-            break
-    if sum > 0:
-        print('NO')
-    elif sum == 0:
-        print('YES')
+    while '()' in b:
+        b = b.replace('()','')
+    print("NO" if b else "YES")
