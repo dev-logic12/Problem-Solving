23d1129f9a0569ff1109c4b9c9627a303fc46fb6
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-12-01 10:09:17 +0900

    [Silver IV] Title: ATM, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,9 +1,11 @@
 n = int(input())
-s = list(map(int, input().split()))
+#기다리는 사람들을 리스트의 형태로 입력 받음 
+people = list(map(int, input().split()))
+#정답 변수의 값을 0으로 초기화 진행 
 num = 0
-s.sort()
+#최솟값 도출을 위해 작은 순서대로(오름차순)으로 정렬 
+people.sort()
 
-for i in range(n):
-  for j in range(i+1):
-     num += s[j]
+for i in range(1, n+1):
+    num += sum(people[0:i])  
 print(num)
