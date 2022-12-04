b02d41ce29274abee9035804c544129e4ebdff4d
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-12-04 11:34:38 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -1,4 +1,6 @@
 def solution(array, height):
-    array.append(height)
-    array.sort(reverse=True)
-    return array.index(height)
+    answer = 0
+    for i in array:
+        if i > height:
+            answer += 1
+    return answer
