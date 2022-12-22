ab5b512b041f9919e73586387f996894c848c12a
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-12-22 21:24:26 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
@@ -1,8 +1,2 @@
-def solution(x):
-    answer = 0
-    for i in str(x):
-        answer += int(i)
-    if x % answer== 0: 
-        return True
-    else: 
-        return False
+def solution(n):
+    return n % sum([int(c) for c in str(n)])== 0
