f1e0ccb64799df8291cda0336aef0addfb7c2dd1
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-12-26 11:00:16 +0900

    [Gold IV] Title: 부분합, Time: 112 ms, Memory: 41836 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -0,0 +1,25 @@
+#부분합 
+import sys
+
+N, S = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+left, right = 0, 0 
+sum = 0
+min_length = sys.maxsize 
+
+while True:
+    if sum >= S:
+        min_length = min(min_length, right - left)
+        sum -= numbers[left]
+        left += 1
+    elif right == N:
+        break
+    else:
+        sum += numbers[right]
+        right += 1
+
+if min_length == sys.maxsize:
+    print(0)
+else:
+    print(min_length)
