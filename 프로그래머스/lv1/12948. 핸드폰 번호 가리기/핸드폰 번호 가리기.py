4cf52e87b78d3f5d8fcff304a53ce82571e6fcc6
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2022-12-28 12:03:42 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,4 +1,1 @@
-def solution(phone_number):
-    # answer = phone_number.replace(phone_number[:-5], '*')
-    answer = "*"*(len(phone_number)-4)
-    return answer + (phone_number[-4:])
+solution = lambda phone_number: "*"*(len(phone_number)-4)+phone_number[-4:]
