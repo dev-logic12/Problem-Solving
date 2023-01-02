4d7369872c6acbd20ade9a81c44a4383c19dc1cb
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2023-01-02 16:05:42 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,2 +1,8 @@
 def solution(n):
-    return ('수박'*n)[:n]
+    answer = ''
+    for i in range(1, n+1):
+        if i % 2 == 0:
+            answer += '박'
+        else:
+            answer += '수'
+    return answer 
