7920634ca3d6fa4c1f9349c0f25a8b26acf140bb
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2023-01-02 15:32:14 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,9 +1,2 @@
-
-def solution(s):
-    answer = ''
-
-    if len(s) % 2 != 0:
-        answer = s[len(s)//2]
-    else: 
-        answer = s[(len(s)//2) - 1] + s[len(s)//2]
-    return answer 
+def solution(str):
+    return str[(len(str)-1)//2:len(str)//2+1]
