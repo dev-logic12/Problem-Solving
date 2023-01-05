23b0832b141711e540ce46964b921136f3dcc8bb
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2023-01-05 12:19:24 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,4 +1,6 @@
 def solution(numbers):
+    #없는 수를 찾아서
+    #더한다 
     num = [1,2,3,4,5,6,7,8,9]
-    answer = list(set(num)-set(numbers))
+    answer = list(set(num)- set(numbers))
     return sum(answer)
