65704e3e38a5587b4b7150284928bed414b897c6
monica Lee <104298444+leesukkyeong@users.noreply.github.com>
2023-01-18 09:33:40 +0900

    [Silver III] Title: 주유소, Time: 124 ms, Memory: 45988 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline 
+
+n = int(input())
+roads = list(map(int, input().split()))
+costs = list(map(int, input().split()))
+
+min_price = roads[0] * costs[0]
+min_cost = costs[0]
+
+for i in range(1, n-1):
+    if min_cost > costs[i]:
+        min_cost = costs[i]
+    min_price += min_cost * roads[i]
+print(min_price)
