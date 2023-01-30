7594d4c08552afde6901ffd451aa71651d367054
monica Lee <monica20144442@gmail.com>
2023-01-30 23:33:27 +0900

    [Bronze V] Title: A+B - 8, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -1,5 +1,5 @@
 t = int(input())
-        
+
 for x in range(1, t+1):
-    a,b = map(int,input().split())
-    print(f'Case #{x}: {a} + {b} = {a+b}')
+    a, b= map(int, input().split())
+    print(f'Case #{x}: {a } + {b} = {a+b}')
