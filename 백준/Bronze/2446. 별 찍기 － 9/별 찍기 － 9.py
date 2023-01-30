327bca66fd77ad3bf9cd67c902768a15a4e74fa4
monica Lee <monica20144442@gmail.com>
2023-01-31 00:35:30 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
@@ -1,5 +1,3 @@
-n = int(input())
-for i in range(1, n+1)[::-1] :  # 1부터 n까지 역순
-    print(' '*(n-i) + '*'*(i*2-1))
-for i in range(2, n+1):  # 2부터 n까지
-    print(' '*(n-i) + '*'*(i*2-1))
+N=int(input())
+for i in range(1,2*N):
+    print(' '*(min(i,2*N-i)-1)+'*'*(1+2*(N-min(2*N-i,i))))
