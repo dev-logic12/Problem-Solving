cd0cb94ed5faf4a9458f2a07a2c358862713ccda
monica Lee <monica20144442@gmail.com>
2023-01-31 09:27:10 +0900

    [Silver III] Title: 1로 만들기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,10 +1,21 @@
-n = int(input())
-d = [0] * (n + 1)	
+x = int(input())
 
-for i in range(2, n + 1):
-    d[i] = d[i - 1] + 1
-    if i % 3 == 0:
-        d[i] = min(d[i], d[i // 3] + 1)	
-    if i % 2 == 0:
-        d[i] = min(d[i], d[i // 2] + 1)
-print(d[n])
+# DP 딕셔너리 초기화 (dp가 1일 때, 연산횟수 0회)
+dp = {1: 0}
+
+# 재귀 사용 (탑다운 방식)
+def cal(n):
+    if n in dp.keys():
+        return dp[n]
+    if (n % 3 == 0) and (n % 2 == 0):
+        dp[n] = min(cal(n // 3) + 1, cal(n // 2) + 1)
+    elif n % 3 == 0:
+        dp[n] = min(cal(n // 3) + 1, cal(n - 1) + 1)
+    elif n % 2 == 0:
+        dp[n] = min(cal(n // 2) + 1, cal(n - 1) + 1)
+    else:
+        dp[n] = cal(n - 1) + 1
+
+    return dp[n]
+
+print(cal(x))
