ad06a9b6775a5eabed6ba4913eea4f835fd1fe27
monica Lee <monica20144442@gmail.com>
2023-02-10 22:47:20 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.py
+++ 프로그래머스/lv1/12982. 예산/예산.py
@@ -1,10 +1,5 @@
 def solution(d, budget):
-    answer = 0
     d.sort()
-    for i in range(len(d)):
-        if d[i] <= budget:
-            answer += 1 
-            budget -= d[i]
-        else:
-            break
-    return answer
+    while budget < sum(d):
+        d.pop()
+    return len(d)
