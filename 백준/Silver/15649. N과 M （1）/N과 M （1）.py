72ab09c8c132172c589c5ed90703aa0c1e891a44
monica Lee <monica20144442@gmail.com>
2023-02-10 15:18:09 +0900

    [Silver III] Title: N과 M (1), Time: 68 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,14 +1,7 @@
-n, m = list(map(int, input().split()))
+from itertools import permutations 
 
-s = []
-def  dfs():
-    if len(s)==m:
-        print(' '.join(map(str, s)))
-        return 
+n,m = map(int, input().split())
+lst = list(map(str, range(1, n+1)))
 
-    for i in range(1, n+1):
-        if i not in s:
-            s.append(i)
-            dfs()
-            s.pop()
-dfs()
+for i in permutations(lst, m):
+    print(' '.join(i))
