bda3461876a41a4a4cdf6cfc9bf1a0c2d565cae7
monica Lee <monica20144442@gmail.com>
2023-02-13 22:32:01 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,12 +1,21 @@
 def solution(n, lost, reserve):
-    reserve_student = set(reserve) - set(lost)
-    
-    lost_student = set(lost) - set(reserve)
-    
-    for student in reserve_student:
-        if (student - 1) in lost_student:
-            lost_student.remove(student - 1)
-        elif (student + 1) in lost_student:
-            lost_student.remove(student + 1)
-            
-    return n - len(lost_student)
+    answer = [1]*(n+2)
+    cnt = 0
+
+    for i in lost:
+        answer[i] -= 1
+    for i in reserve: 
+        answer[i] += 1 
+
+    for i in range(1, n+1):
+        if answer[i] == 2:
+            if answer[i-1] == 0:
+                answer[i] -= 1
+                answer[i-1] += 1
+            elif answer[i+1] == 0:
+                answer[i] -= 1
+                answer[i+1] += 1
+    for i in range(1, n+1):
+        if answer[i] >= 1:
+            cnt += 1 
+    return cnt 
