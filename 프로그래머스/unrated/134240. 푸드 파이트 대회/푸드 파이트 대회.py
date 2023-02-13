cf5471545ead42944436cfdd381bb4e7a798d9bc
monica Lee <monica20144442@gmail.com>
2023-02-13 10:36:28 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -2,5 +2,4 @@ def solution(food):
     answer = ''
     for i in range(1, len(food)):
         answer += str(i)*(food[i]//2)
-    temp = ''.join(reversed(list(answer)))
-    return answer +'0'+ temp
+    return answer +'0'+ answer[::-1]
