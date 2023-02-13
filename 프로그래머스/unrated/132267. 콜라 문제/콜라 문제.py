81f85bee7fb4fe517cd2678a330dd102546ccc4f
monica Lee <monica20144442@gmail.com>
2023-02-13 09:59:04 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -1,8 +1,9 @@
 def solution(a, b, n):
     answer = 0
-    while (n>= a):
-        remain_bottle = n%a 
-        n = (n//a)*b 
-        answer += n 
-        n += remain_bottle
-    return answer 
+    # 1. 빈 병의 개수 n이 교환 가능한 최소 숫자 a 이상일 때까지 반복
+    while n >= a:
+        # 2. b개의 병 추가
+        answer += b
+        # 3. 남은 병 계산하기
+        n = n - a + b
+    return answer
