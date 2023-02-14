fc053bef361ad28433e8010ebfb587b5055b66fe
monica Lee <monica20144442@gmail.com>
2023-02-14 09:01:56 +0900

    [level 1] Title: 폰켓몬, Time: 0.18 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,7 @@
 def solution(nums):
-    num = set(nums)
-    for i in nums:
-        if len(num) >= (len(nums)//2):
-            return (len(nums)//2)
-        else: 
-            return len(num)
+    answer = len(set(nums))
+    if len(nums) / 2 > answer:
+        return answer 
+    else:
+        return len(nums) / 2
+        
