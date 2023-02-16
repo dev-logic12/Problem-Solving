d663741e907c63537b673050b748c5911fb5c668
monica Lee <monica20144442@gmail.com>
2023-02-16 14:43:06 +0900

    [level 1] Title: 모의고사, Time: 2.45 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,26 +1,27 @@
 def solution(answers):
-    a1 = [1,2,3,4,5]
-    a2 = [2,1,2,3,2,4,2,5]
-    a3 = [3,3,1,1,2,2,4,4,5,5]
+    a = [1,2,3,4,5]
+    b = [2,1,2,3,2,4,2,5]
+    c = [3,3,1,1,2,2,4,4,5,5]
     c1, c2, c3 = 0, 0, 0
     cnt = []
+    answer = []
     for i in range(len(answers)):
-        s1 = i%5 
+        s1 = i%5
         s2 = i%8
         s3 = i%10 
 
-        if a1[s1] == answers[i]:
-            c1 += 1 
-        if a2[s2] == answers[i]:
+        if answers[i] == a[s1]:
+            c1 += 1
+        if answers[i] == b[s2]:
             c2 += 1 
-        if a3[s3] == answers[i]:
+        if answers[i] == c[s3]:
             c3 += 1 
     answer = max(c1, c2, c3)
 
     if answer == c1:
         cnt.append(1)
-    if answer == c2:
+    if answer == c2: 
         cnt.append(2)
-    if answer == c3:
+    if answer == c3: 
         cnt.append(3)
     return cnt 
