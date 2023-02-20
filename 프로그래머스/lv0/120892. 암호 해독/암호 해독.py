c2a1af1fcce84c92131b3e47e87be90dd0804db7
monica Lee <monica20144442@gmail.com>
2023-02-20 20:40:26 +0900

    [level 0] Title: 암호 해독, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,2 +1,6 @@
 def solution(cipher, code):
-    return cipher[code-1::code]
+    answer = ''
+    for i in range(0, len(cipher)):
+        if (i + 1) % code == 0:
+            answer += cipher[i]
+    return answer
