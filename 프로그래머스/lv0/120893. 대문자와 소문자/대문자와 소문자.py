bdfe9fe57c58d0d05e56c2048a4cf00078c74f73
monica Lee <monica20144442@gmail.com>
2023-02-20 16:35:10 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.42 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,8 +1,8 @@
 def solution(my_string):
-    answer = ''
-    for i in my_string:
-        if i.isupper():
-            answer += i.lower()
-        else:
-            answer += i.upper()
-    return answer
+  answer = ''
+  for i in range(len(my_string)):
+    if my_string[i].isupper() == True:
+      answer += my_string[i].lower()
+    else:
+      answer += my_string[i].upper()
+  return answer 
