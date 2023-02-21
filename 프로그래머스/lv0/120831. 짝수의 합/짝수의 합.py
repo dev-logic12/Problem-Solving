4f40290cdc89550b08a2ae2179de638ecc077665
monica Lee <monica20144442@gmail.com>
2023-02-22 07:44:04 +0900

    [level 0] Title: 짝수의 합, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.py
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.py
@@ -1,5 +1,6 @@
 def solution(n):
-    s = 0
-    for i in range(2, n+1, 2):
-        s += i
-    return s 
+  answer = 0 
+  for i in range(0, n+1, 2):
+    if i % 2  == 0:
+      answer += i 
+  return answer
