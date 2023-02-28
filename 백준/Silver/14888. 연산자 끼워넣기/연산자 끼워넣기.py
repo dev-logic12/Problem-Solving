c53e72d309c760e140f01039339efb3cb1194784
monica Lee <monica20144442@gmail.com>
2023-02-28 15:19:05 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 572 ms, Memory: 36376 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,29 +1,35 @@
-import sys
 from itertools import permutations
 
-N = int(sys.stdin.readline())
-A = list(map(int, sys.stdin.readline().split()))
-plus, minus, mul, div = map(int, sys.stdin.readline().split())
+n = int(input())
+a = list(map(int, input().split()))
+op = list(map(int, input().split()))
 
-st = "+ " * plus + "- " * minus + "* " * mul + "// " * div
-li = st.split()
-test = set(permutations(li, len(li)))
+# 모든 연산자 조합 생성
+op_list = []
+for i in range(4):
+    op_list.extend([i]*op[i])
+perm_op = set(permutations(op_list, n-1))
 
-res = list()
-for i in test:
-    sum = A[0]
-    for j in range(len(i)):
-        if i[j] == "+":
-            sum += A[j + 1]
-        elif i[j] == "-":
-            sum -= A[j + 1]
-        elif i[j] == "*":
-            sum *= A[j + 1]
-        elif i[j] == "//":
-            if sum < 0:
-                sum = -(abs(sum) // A[j + 1])
+min_ans = float('inf')
+max_ans = float('-inf')
+
+# 모든 연산자 조합에대해 계산하여 최댓값과 최솟값 찾기
+for p in perm_op:
+    cur = a[0]
+    for i in range(n-1):
+        if p[i] == 0:
+            cur += a[i+1]
+        elif p[i] == 1:
+            cur -= a[i+1]
+        elif p[i] == 2:
+            cur *= a[i+1]
+        else:
+            if cur < 0:
+                cur = -(-cur//a[i+1])
             else:
-                sum //= A[j + 1]
-    res.append(sum)
+                cur //= a[i+1]
+    max_ans = max(max_ans, cur)
+    min_ans = min(min_ans, cur)
 
-print(max(res), min(res))
+print(max_ans)
+print(min_ans)
