696436b94b3da0c3a5fe7c79e16e4a614250a69b
monica Lee <monica20144442@gmail.com>
2023-03-03 13:36:12 +0900

    [level 2] Title: 피로도, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -0,0 +1,19 @@
+answer = 0 
+n = 0 
+visited = []
+
+def dfs(k, cnt, dungeons):
+    global answer 
+    if cnt > answer:
+        answer = cnt 
+    for j in range(n):
+        if k >= dungeons[j][0] and not visited[j]:
+            visited[j] = 1 
+            dfs(k-dungeons[j][1], cnt +1, dungeons)
+            visited[j] = 0 
+def solution(k, dungeons):
+    global n, visited
+    n = len(dungeons)
+    visited = [0] * n 
+    dfs(k, 0, dungeons)
+    return answer
