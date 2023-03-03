dfc1080993705e95a5fa70d88a5bac9f447575e6
monica Lee <monica20144442@gmail.com>
2023-03-03 15:25:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.36 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,3 +1,3 @@
 from collections import Counter
 def solution(participant, completion):
-    return list(Counter(participant)- Counter(completion))[0]
+    return list(Counter(participant) - Counter(completion))[0]
