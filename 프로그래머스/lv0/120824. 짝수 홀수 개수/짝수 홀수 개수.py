2233c7c7dac6f2cba029b39eefa6c4c1df238d83
monica Lee <monica20144442@gmail.com>
2023-03-08 15:43:38 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,3 +1,37 @@
 def solution(num_list):
-    even = list(filter(lambda x : x % 2 == 0, num_list))
-    return [len(even), len(num_list)-len(even)]
+    a = []
+    b = []
+    for i in range(len(num_list)):
+        if num_list[i] %2 == 0:
+            a.append(i)
+        else: 
+            b.append(i)
+    return [len(a), len(b)]
+
+'''
+
+def solution(num_list):
+    a = []
+    b = []
+    for i in range(len(num_list)):
+        if num_list[i] % 2 == 0 :
+            a.append(i)
+        else:
+            b.append(i)
+
+    return [len(a),len(b)]
+
+
+
+
+def solution(num_list):
+    answer = [0,0]
+
+    for num in num_list:
+        if num % 2 == 0:
+            answer[0] += 1
+        else:
+            answer[1] += 1
+
+    return answer
+'''
