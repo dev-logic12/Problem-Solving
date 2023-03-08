e4d32f053ca1dfb9e15963861dd6496157a382d7
monica Lee <monica20144442@gmail.com>
2023-03-08 17:13:50 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -1,7 +1,3 @@
 def solution(my_string, n):
-    text = []
-    for word in my_string:
-        for _ in range(n):
-            text.append(word)
-    answer = "".join(text)
-    return answer
+    return ''.join(i*n for i in my_string)
+
