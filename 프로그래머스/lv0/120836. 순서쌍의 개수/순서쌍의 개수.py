f096d4b321cb8ec16a05060dc1a69cd548fe6c1b
monica Lee <monica20144442@gmail.com>
2023-03-08 17:02:24 +0900

    [level 0] Title: 순서쌍의 개수, Time: 69.72 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,6 +1,6 @@
 def solution(n):
-    answer = 0
+    answer = 0 
     for i in range(n):
-        if n % (i + 1) == 0:
-            answer += 1
+        if n %(i+1) == 0:
+            answer += 1 
     return answer 
