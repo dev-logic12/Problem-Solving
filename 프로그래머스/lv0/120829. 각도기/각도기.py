fac21485b1831133e117c7aa1a26e7a024fedfa4
monica Lee <monica20144442@gmail.com>
2023-03-08 15:02:12 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.py
+++ 프로그래머스/lv0/120829. 각도기/각도기.py
@@ -1,9 +1,9 @@
 def solution(angle):
-  if angle == 180:
-    return 4
-  elif 180 > angle > 90:
-    return 3
-  elif angle == 90:
-    return 2
-  elif 90 > angle > 0:
-    return 1
+    if angle == 180:
+        return 4
+    if 90 < angle < 180:
+        return 3
+    if angle == 90:
+        return 2
+    if 0<angle < 90:
+        return 1 
