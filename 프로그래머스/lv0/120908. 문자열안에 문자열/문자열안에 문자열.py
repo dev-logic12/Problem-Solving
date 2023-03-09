8e05aada1cd58c29f16c48c5c45d4ad1eef5327d
monica Lee <monica20144442@gmail.com>
2023-03-09 13:49:39 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.00 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.py
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.py
@@ -1,7 +1,5 @@
 def solution(str1, str2):
-  answer = 0 
-  for i in range(len(str1)):
-    if str2 in str1: 
-      return 1 
+    if str2 in str1:
+        return 1
     else:
-      return 2
+        return 2
