89b50626bd3647f9a1ccdecefba31853bd34ecb9
monica Lee <monica20144442@gmail.com>
2023-03-11 19:45:35 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,27 +1,29 @@
+'''
+기본적으로 1을 담고 
+로스트 뺴고 
+리절브 더하고 
+카운트를 계산해서 더하기 
+'''
+
+
+
 def solution(n, lost, reserve):
     answer = [1]*(n+2)
-    cnt = 0 
-    '''
-    기본적으로 1 넣고 
-    로스트 빼고 
-    리절브 더하고 
-    양옆에 줄 수 있으면 주고 
-    카운트로 세기 
-    '''
+    cnt = 0
     for i in range(1, n+1):
         if i in lost:
             answer[i] -= 1 
-        if i in reserve:
+        if i in reserve: 
             answer[i] += 1 
     for i in range(1, n+1):
         if answer[i] == 2:
-            if answer[i-1] ==0:
-                answer[i] -= 1 
+            if answer[i-1] == 0:
+                answer[i] -= 1
                 answer[i-1] += 1 
             elif answer[i+1] == 0:
                 answer[i] -= 1 
-                answer[i+1] += 1
+                answer[i+1] += 1 
     for i in range(1, len(answer)-1):
         if answer[i] >= 1:
             cnt += 1 
-    return cnt 
+    return cnt
