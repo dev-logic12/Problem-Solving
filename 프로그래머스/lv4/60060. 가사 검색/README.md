172b56081b06a8f9b7ad078b968da7e7234f265f
monica Lee <monica20144442@gmail.com>
2023-03-17 12:51:52 +0900

    [level 4] Title: 가사 검색, Time: 4566.66 ms, Memory: 672 MB -BaekjoonHub

--- 프로그래머스/lv4/60060. 가사 검색/README.md
+++ 프로그래머스/lv4/60060. 가사 검색/README.md
@@ -1,10 +1,10 @@
 # [level 4] 가사 검색 - 60060 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60060) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60060?language=python3) 
 
 ### 성능 요약
 
-메모리: 631 MB, 시간: 2374.90 ms
+메모리: 672 MB, 시간: 4566.66 ms
 
 ### 구분
 
--- 프로그래머스/lv4/60060. 가사 검색/가사 검색.py
+++ 프로그래머스/lv4/60060. 가사 검색/가사 검색.py
@@ -1,44 +1,47 @@
+import sys
+sys.setrecursionlimit(100001)
 def solution(words, queries):
-    head, head_rev = {}, {}
-    wc = []
+    answer = []
     
-    def add(head,word):
-        node = head
-        for w in word:
-            if w not in node:
-                node[w]={}
-            node= node[w]
-            if 'len' not in node:
-                node['len'] = [len_word]
-            else:
-                node['len'].append(len_word)
-        node['end']=True   
-    
-    for word in words:
-        len_word = len(word)
-        add(head,word)
-        add(head_rev,word[::-1])
-        wc.append(len_word)
-        
-    def search(head, querie):
-        count=0
-        node = head
-        for q in querie:
-            if q=='?':
-                return node['len'].count(len_qu)
-            elif q not in node:
-                break
-            node = node[q]
-        return count
+    rev_words, counted = [], []   # 조건 b, c를 위한 두 변수
+    for w in words:
+        rev_words.append(w[::-1])
+        counted.append(len(w))
 
-    li=[]
-    for querie in queries:
-        len_qu = len(querie)
-        if querie[0]=='?':
-            if querie[-1]=='?': 
-                li.append(wc.count(len_qu))
-            else: 
-                li.append(search(head_rev, querie[::-1]))
-        else:
-            li.append(search(head, querie))
-    return li
+    trie = make_trie({}, words)   # 조건 a 의 trie
+    rev_trie = make_trie({}, rev_words)   # 조건 b 의 rev_trie 
+
+    for query in queries:  # 3가지 조건으로 나누어서,
+        if query[0] == '?' and query[-1] == '?':
+            answer.append(counted.count(len(query)))
+        elif query[0] == '?':
+            answer.append(search_trie(rev_trie, query[::-1], len(query)))
+        elif query[-1] == '?':
+            answer.append(search_trie(trie, query, len(query)))
+
+    return answer
+
+
+def make_trie(trie, words):
+    for word in words:
+        cur = trie
+        l = len(word)
+        for w in word:
+            if w in cur:
+                cur = cur[w]
+                cur['!'].append(l)
+            else:
+                cur[w] = {}
+                cur = cur[w]
+                cur['!'] = [l]
+    return trie
+
+
+def search_trie(trie, query, length):
+    count = 0
+    if query[0] == '?':
+        return trie['!'].count(length)
+    elif query[0] in trie:
+        count += search_trie(trie[query[0]], query[1:], length)
+
+    return count
