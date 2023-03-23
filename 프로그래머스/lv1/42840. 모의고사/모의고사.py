bc70f0736548f6696f6254353b9eb249aa1b4a54
monica Lee <monica20144442@gmail.com>
2023-03-23 09:54:06 +0900

    [level 1] Title: 모의고사, Time: 2.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,20 +1,22 @@
 def solution(answers):
-    pattern1 = [1,2,3,4,5]
-    pattern2 = [2,1,2,3,2,4,2,5]
-    pattern3 = [3,3,1,1,2,2,4,4,5,5]
-    score = [0, 0, 0]
-    result = []
+    answer = []
+    p1 = [1,2,3,4,5] * 2000
+    p2 = [2,1,2,3,2,4,2,5] * 1250
+    p3 = [3,3,1,1,2,2,4,4,5,5] * 1000
+    c1, c2, c3 = 0, 0, 0
 
-    for idx, answer in enumerate(answers):
-        if answer == pattern1[idx%len(pattern1)]:
-            score[0] += 1 
-        if answer == pattern2[idx%len(pattern2)]:
-            score[1] += 1
-        if answer == pattern3[idx%len(pattern3)]:
-            score[2] += 1 
-
-    for idx, s in enumerate(score):
-        if s == max(score):
-            result.append(idx+1)
-            
-    return result 
+    for i in range(len(answers)):
+        if p1[i] == answers[i]:
+            c1 += 1 
+        if p2[i] == answers[i]:
+            c2 += 1 
+        if p3[i] == answers[i]:
+            c3 += 1 
+    max_count = max(c1, c2, c3)
+    if max_count == c1:
+        answer.append(1)
+    if max_count == c2:
+        answer.append(2)
+    if max_count == c3:
+        answer.append(3)
+    return answer
