49864ef377677ba6969fd39b1fdac7b6923ad6ef
monica Lee <monica20144442@gmail.com>
2023-03-23 10:15:45 +0900

    [level 2] Title: 소수 찾기, Time: 4.97 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,23 +1,12 @@
-
-from itertools import permutations 
-
-def solution(numbers):
-    answer = []
-    nums = [n for n in numbers]
-    per = []
-    for i in range(1, len(numbers)+1):
-        per += list(permutations(nums, i))
-    new_nums = [int(("").join(p)) for p in per]
+from itertools import permutations
+def solution(n):
+    a = set()
     
-    for n in new_nums:
-        if n <2:
-            continue
-        check = True
-        for i in range(2, int(n**0.5)+1):
-            if n % i ==0:
-                check = False
-                break
-        if check:
-            answer.append(n)
-    return len(set(answer))
-    return answer
+    for i in range(len(n)):
+        a |= set(map(int, map("".join, permutations(list(n), i + 1))))
+    a -= set(range(0, 2))
+    
+    for i in range(2, int(max(a) ** 0.5) + 1):
+        a -= set(range(i * 2, max(a) + 1, i))
+        
+    return len(a) 
