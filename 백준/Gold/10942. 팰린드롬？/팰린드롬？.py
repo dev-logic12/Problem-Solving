e57bc969779eb5d4633fbad1c9c0ab15afe88132
monica Lee <monica20144442@gmail.com>
2023-04-03 11:03:14 +0900

    [Gold IV] Title: 팰린드롬?, Time: 2212 ms, Memory: 61956 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -0,0 +1,30 @@
+import sys
+
+# 입력 받기
+N = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+M = int(sys.stdin.readline())
+
+# dp[i][j]는 arr[i] ~ arr[j]가 팰린드롬인지 여부를 저장합니다.
+dp = [[0] * N for _ in range(N)]
+
+# 1개짜리 문자열은 모두 팰린드롬입니다.
+for i in range(N):
+    dp[i][i] = 1
+
+# 2개짜리 문자열도 팰린드롬인지 체크합니다.
+for i in range(N-1):
+    if arr[i] == arr[i+1]:
+        dp[i][i+1] = 1
+
+# 3개 이상의 문자열에 대해 팰린드롬 여부를 체크합니다.
+for k in range(2, N):
+    for i in range(N-k):
+        j = i + k
+        if arr[i] == arr[j] and dp[i+1][j-1] == 1:
+            dp[i][j] = 1
+
+# M개의 질문에 대해 답을 출력합니다.
+for _ in range(M):
+    start, end = map(int, sys.stdin.readline().split())
+    print(dp[start-1][end-1])
