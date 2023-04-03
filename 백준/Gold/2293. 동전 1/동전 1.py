d9354b1e20f3382d6db508d8af9744b6343b1262
monica Lee <monica20144442@gmail.com>
2023-04-03 11:04:21 +0900

    [Gold V] Title: 동전 1, Time: 192 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,10 +1,19 @@
-n, k = map(int, input().split())
-c =[int(input()) for i in range(n)]
-dp = [0 for i in range(k+1)]
-dp[0] = 1 
+import sys
 
-for i in c:
-    for j in range(i, k+1):
-        if j-i >= 0:
-            dp[j] += dp[j-i]
+# 입력 받기
+n, k = map(int, sys.stdin.readline().split())
+coins = []
+for _ in range(n):
+    coins.append(int(sys.stdin.readline()))
+
+# dp[i]는 i원을 만드는 경우의 수를 저장합니다.
+dp = [0] * (k+1)
+dp[0] = 1
+
+# 동전의 가치를 하나씩 확인하면서 dp 테이블을 갱신합니다.
+for coin in coins:
+    for i in range(coin, k+1):
+        dp[i] += dp[i-coin]
+
+# k원을 만드는 경우의 수 출력
 print(dp[k])
