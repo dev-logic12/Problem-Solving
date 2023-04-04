1510b249d4a99c1d0c823c2e77db7ecac6756c8f
monica Lee <monica20144442@gmail.com>
2023-04-04 10:05:36 +0900

    [level 2] Title: 올바른 괄호, Time: 12.44 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,13 +1,13 @@
-
-
 def solution(s):
     stack = []
     for i in s:
         if i == '(':
             stack.append(i)
-        else:
-            if stack == []:
-                return False
-            else:
-                stack.pop()
-    return stack ==[]
+        if i == ')':
+            if len(stack) == 0 or stack[-1] != '(':
+                return False 
+            stack.pop()
+    if len(stack) == 0:
+        return True
+    else:
+        return False
