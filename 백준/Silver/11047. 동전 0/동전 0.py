e43ca4f89406f81e9f9c6e7654b202b6f9f07ca5
monica Lee <monica20144442@gmail.com>
2023-04-12 10:16:11 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,11 +1,15 @@
-N, K = map(int, input().split()) 
-coin_lst = list()
-for i in range(N):
-    coin_lst.append(int(input()))
+n, k = map(int, input().split())
+coins = []  # 동전의 가치를 저장할 리스트
+cnt = 0 
 
-count = 0
-for i in reversed(range(N)):
-    count += K//coin_lst[i] 
-    K = K%coin_lst[i] 
+for i in range(n):
+    coin = int(input())  # 동전의 가치를 입력받음
+    coins.append(coin)  # 동전의 가치를 coins 리스트에 저장
+coins.sort(reverse=True)  # coins 리스트를 가치가 큰 순서대로 정렬
 
-print(count)
+for coin in coins:
+    if coin <= k:
+        cnt += k // coin  # 현재 동전으로 만들 수 있는 개수를 cnt에 더함
+        k %= coin  # k를 현재 동전으로 만들고 남은 나머지로 갱신
+
+print(cnt)
