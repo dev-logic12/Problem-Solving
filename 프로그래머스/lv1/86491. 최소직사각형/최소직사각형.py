573b0aafc027508010b7b5f58bcc426b46a723ed
monica Lee <monica20144442@gmail.com>
2023-04-15 08:41:06 +0900

    [level 1] Title: 최소직사각형, Time: 3.47 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -1,11 +1,20 @@
 def solution(sizes):
-    small = []
-    big = []
-    for i in range(len(sizes)):
-        if sizes[i][0] >= sizes[i][1]:
-            small.append(sizes[i][1])
-            big.append(sizes[i][0])
-        else:
-            small.append(sizes[i][0])
-            big.append(sizes[i][1])
-    return max(small)*max(big)
+    max_width = 0
+    max_height = 0
+    
+    for size in sizes:
+        # 각 사각형의 가로와 세로 중 최댓값과 최솟값을 구함
+        width = max(size[0], size[1])
+        height = min(size[0], size[1])
+        
+        # 현재 사각형의 가로와 세로가 기존의 최댓값보다 크다면 갱신
+        if width > max_width:
+            max_width = width
+        # 현재 사각형의 세로가 기존의 최댓값보다 크다면 갱신
+        if height > max_height:
+            max_height = height
+    
+    # 최댓값들을 곱하여 최소직사각형의 넓이를 구함
+    answer = max_width * max_height
+    return answer
+
