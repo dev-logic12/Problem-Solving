fc3ce9ef2fa9a29393a4bae55db842eb4733fdaf
monica Lee <monica20144442@gmail.com>
2023-04-19 10:11:05 +0900

    [level 2] Title: 양궁대회, Time: 636.95 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
@@ -1,53 +1,40 @@
-from collections import deque
-
-def bfs(n, info):    
-    res = []
-    q = deque([(0, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])])
-    maxGap = 0
-    
-    while q:
-        focus, arrow = q.popleft()
-        
-        if sum(arrow) == n:  # 종료조건 1) 화살 다 쏜 경우
-            apeach, lion = 0, 0
-            for i in range(11):
-                if not (info[i] == 0 and arrow[i] == 0):
-                    if info[i] >= arrow[i]:
-                        apeach += 10 - i
-                    else:
-                        lion += 10 - i
-            if apeach < lion:  # 라이언이 이기면
-                gap = lion - apeach
-                if maxGap > gap:
-                    continue
-                if maxGap < gap:
-                    maxGap = gap  # 최대점수차 갱신
-                    res.clear()
-                res.append(arrow)  # 최대점수차를 내는 화살상황 저장
-        
-        elif sum(arrow) > n:  # 종료조건 2) 화살 더 쏜 경우
-            continue
-        
-        elif focus == 10:  # 종료조건 3) 화살 덜 쏜 경우
-            tmp = arrow.copy()
-            tmp[focus] = n - sum(tmp)
-            q.append((-1, tmp))
-        
-        else:  # 화살 쏘기
-            tmp = arrow.copy()
-            tmp[focus] = info[focus]+1 
-            q.append((focus+1, tmp))  # 어피치보다 1발 많이 쏘기
-            tmp2 = arrow.copy()
-            tmp2[focus] = 0
-            q.append((focus+1, tmp2))  # 0발 쏘기
-    return res
-
 def solution(n, info):
-    winList = bfs(n, info)
-    
-    if not winList:
-        return [-1]
-    elif len(winList) == 1:
-        return winList[0]
-    else:
-        return winList[-1]
+    global best_archery, best_score
+
+    # 라이언이 해당 종류의 과녁에서 얻은 점수를 계산하는 함수
+    def calculate_score(archery):
+        score = 0
+        for i in range(11):
+            if archery[i] == info[i] == 0:
+                continue
+            if archery[i] > info[i]:
+                score += 10 - i
+            else:
+                score -= 10 - i
+        return score
+
+    # 백트래킹으로 가능한 모든 라이언의 화살 개수 조합을 생성하는 함수
+    def dfs(idx, arrows_left, archery):
+        global best_archery, best_score
+        if idx == -1 and arrows_left:
+            return
+        if arrows_left == 0:
+            score = calculate_score(archery)
+            if best_score < score:
+                best_archery = archery[:]
+                best_score = score
+            return
+        for i in range(arrows_left, -1, -1):
+            archery[idx] = i
+            dfs(idx-1, arrows_left-i, archery)
+            archery[idx] = 0
+
+    # 최고 점수를 가지는 조합을 찾기 위한 초기화
+    best_archery = [0 for _ in range(11)]
+    best_score = 0
+
+    # dfs 함수 호출
+    dfs(10, n, [0 for _ in range(11)])
+
+    # 결과 반환
+    return best_archery if best_score != 0 else [-1]
