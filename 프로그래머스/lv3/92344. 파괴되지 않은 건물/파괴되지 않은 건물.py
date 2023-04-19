d2209590a52b220485130dbde6a20e155f1b85d7
monica Lee <monica20144442@gmail.com>
2023-04-19 10:28:14 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 690.40 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/lv3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/lv3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,30 +1,30 @@
 def solution(board, skill):
-    answer = 0
-    tmp = [[0] * (len(board[0]) + 1) for _ in range(len(board) + 1)] # 누적합 기록을 위한 배열
- 
-    for type, r1, c1, r2, c2, degree in skill:
-        # 누적합 기록, 부호에 주의할 것
-        tmp[r1][c1] += degree if type == 2 else -degree
-        tmp[r1][c2 + 1] += -degree if type == 2 else degree
-        tmp[r2 + 1][c1] += -degree if type == 2 else degree
-        tmp[r2 + 1][c2 + 1] += degree if type == 2 else -degree
- 
-    # 행 기준 누적합
-    for i in range(len(tmp) - 1):
-        for j in range(len(tmp[0]) - 1):
-            tmp[i][j + 1] += tmp[i][j]
- 
-    # 열 기준 누적합
-    for j in range(len(tmp[0]) - 1):
-        for i in range(len(tmp) - 1):
-            tmp[i + 1][j] += tmp[i][j]
- 
-    # 기존 배열과 합함
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            board[i][j] += tmp[i][j]
-            # board에 값이 1이상인 경우 answer++
-            if board[i][j] > 0: answer += 1
- 
-    return answer
- 
+    # 보드 크기 설정
+    R, C = len(board) + 1, len(board[0]) + 1
+    # diff 리스트 초기화
+    diff = [[0] * C for _ in range(R)]
+
+    # 각 스킬 적용
+    for t, r1, c1, r2, c2, d in skill:
+        d *= -1 if t == 1 else 1
+        set_diff(diff, r1, c1, r2, c2, d)
+
+    # diff 리스트 누적합 계산
+    for i in range(1, R):
+        diff[i][0] += diff[i - 1][0]
+        diff[0][i] += diff[0][i - 1]
+
+    # 보드의 양수 칸 개수 계산
+    ans = 0
+    for i in range(1, R):
+        for j in range(1, C):
+            diff[i][j] += diff[i - 1][j] + diff[i][j - 1] - diff[i - 1][j - 1]
+            ans += 1 if board[i - 1][j - 1] + diff[i - 1][j - 1] > 0 else 0
+    return ans
+
+def set_diff(diff, r1, c1, r2, c2, d):
+    # diff 리스트 갱신
+    diff[r1][c1] += d
+    diff[r2 + 1][c2 + 1] += d
+    diff[r2 + 1][c1] -= d
+    diff[r1][c2 + 1] -= d
