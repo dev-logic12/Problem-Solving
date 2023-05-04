e220dd4a078c6e15afe0d1b3b72cbbc5e717dc7d
monica Lee <monica20144442@gmail.com>
2023-05-04 10:28:46 +0900

    [unrated] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
@@ -0,0 +1,22 @@
+def solution(arr, n):
+    answer = []
+    for i in range(len(arr)):
+        if len(arr) % 2 == 1:  # arr의 길이가 홀수인 경우
+            if i % 2 == 0:  # 짝수 인덱스에 대해
+                answer.append(arr[i] + n)
+            else:
+                answer.append(arr[i])
+        else:  # arr의 길이가 짝수인 경우
+            if i % 2 == 1:  # 홀수 인덱스에 대해
+                answer.append(arr[i] + n)
+            else:
+                answer.append(arr[i])
+    return answer
+
+
+
+
+
+# arr = [49, 12, 100, 276, 33]	
+# n = 27
+# print(solution(arr, n))
