70ed2ee04b410c712c9721dd0b884dad0e09f798
monica Lee <monica20144442@gmail.com>
2023-05-05 10:15:01 +0900

    [unrated] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.py
@@ -0,0 +1,12 @@
+def solution(my_string, alp):
+    answer = ''
+    for i in my_string:
+        if i in alp:
+            answer += i.upper()
+        else:
+            answer += i 
+    return answer
+
+# my_string = "programmers"	
+# alp = "P"	
+# print(solution(my_string, alp))
