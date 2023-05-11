c26ab4bef98bc2eda727fe4a93fcc579cc874215
monica Lee <monica20144442@gmail.com>
2023-05-11 10:26:32 +0900

    [unrated] Title: ad 제거하기, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.py
@@ -0,0 +1,9 @@
+def solution(strArr):
+    answer = []
+    for word in strArr:
+        if 'ad' not in word:
+            answer.append(word)
+    return answer
+
+# strArr = ["and","notad","abcd"]	
+# print(solution(strArr))
