e021b59bb60f7ed4c4cdad5da9f501b716cf46d3
monica Lee <monica20144442@gmail.com>
2023-05-15 22:25:11 +0900

    [unrated] Title: 억억단을 외우자, Time: 9893.72 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/unrated/138475. 억억단을 외우자/억억단을 외우자.py
+++ 프로그래머스/unrated/138475. 억억단을 외우자/억억단을 외우자.py
@@ -1,22 +1,14 @@
 def solution(e, starts):
-    divisor_list = [0]*(e+1)
-    min_s = min(starts)
-    for i in range(1, e + 1):
-        if i * i <= e:
-            divisor_list[i*i] += 1
-        for j in range(i+1, e + 1):
-            n = i * j
-            if n > e:
-                break
-            divisor_list[n] += 2
-            
-    max_div_list = [0]*(e+1)
-    max_div_list[-1] = e
-    for i in range(e-1, min_s-1, -1) :
-        if divisor_list[max_div_list[i+1]] <= divisor_list[i] :
-            max_div_list[i] = i
-        else :
-            max_div_list[i] = max_div_list[i+1]
-    answer = [ max_div_list[s] for s in starts ]
-    
-    return answer
+    arr = [1]*(e+1)
+    ans = [0]*(e+1)
+    i = 2
+    for i in range(2,e+1):
+        for j in range(i*2,e+1,i):
+            arr[j] += 1
+    ans[e] = e
+    for i in reversed(range(0,e)):
+        if arr[i] >= arr[ans[i+1]]:
+            ans[i] = i
+        else:
+            ans[i] = ans[i+1]
+    return [ans[i] for i in starts]
