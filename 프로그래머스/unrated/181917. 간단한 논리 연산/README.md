215ccec5073dffde38729e3bafab8d3f75638361
monica Lee <monica20144442@gmail.com>
2023-05-18 10:05:14 +0900

    [unrated] Title: 간단한 논리 연산, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/README.md
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/README.md
@@ -1,10 +1,10 @@
 # [unrated] 간단한 논리 연산 - 181917 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181917) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181917?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.03 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.py
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.py
@@ -1,22 +1,2 @@
 def solution(x1, x2, x3, x4):
-    return eval(f"({x1} or {x2}) and ({x3} or {x4})")
-
-
-# def solution(x1, x2, x3, x4):
-#     answer = []
-#     answer1 = []
-#     answer2 = []
-#     answer.append(x1)
-#     answer.append(x1)
-#     answer.append(x3)
-#     answer.append(x4)
-
-#     for i in answer:
-#         if i == True:
-#             answer1.append(i)
-#         elif i == False:
-#             answer2.append(i)
-#     if len(answer1) > len(answer2):
-#         return True
-#     else:
-#         return False
+    return (x1 or x2) and (x3 or x4)
