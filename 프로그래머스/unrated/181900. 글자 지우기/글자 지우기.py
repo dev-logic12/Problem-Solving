f9056d8b556ad84df3d97c904d4cd6c8d7185f19
monica Lee <monica20144442@gmail.com>
2023-05-23 10:00:32 +0900

    [unrated] Title: 글자 지우기, Time: 0.11 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.py
@@ -0,0 +1,7 @@
+def solution(my_string, indices):
+    answer = ''
+    
+    for index, char in enumerate(my_string):
+        if index not in indices:
+            answer += char
+    return answer
