611e6e44bb6466f426dbbd02debb3363c8e0b382
monica Lee <monica20144442@gmail.com>
2023-05-27 18:58:10 +0900

    [unrated] Title: 5명씩, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.py
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.py
@@ -1,9 +1,2 @@
 def solution(names):
-    answer = []
-    answer1 = []
-    chunk_size = 5
-    for i in range(0, len(names), chunk_size):
-        answer.append(names[i:i+chunk_size])        
-    for sublist in answer:
-        answer1.append(sublist[0])
-    return answer1
+    return names[::5]
