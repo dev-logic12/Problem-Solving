42dbb0695d4fc7c14e146f68ac25e68af57ecdd9
monica Lee <monica20144442@gmail.com>
2023-06-07 10:21:47 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 132 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
@@ -1,11 +1,12 @@
 n = int(input())
+cnt = 0 
 
-answer = 0 
-for a in range(1, n+1): #택희 
-    for b in range(1, n+1): #영훈
-        for c in range(1, n+1): #남규 
-            if (a + b + c == n):
-                if (c > b+2):
-                    if (a%2 ==1):
-                        answer += 1
-print(answer)
+for a in range(1, n+1):
+    for b in range(1,n+1):
+        for c in range(1, n+1):
+            if a+b+c == n:
+                if c >= b+2:
+                    if a%2 == 0:
+                        cnt +=1
+
+print(cnt)
