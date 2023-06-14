cdf10cc5dce9278e5c294a74deeab1bc725774af
monica Lee <monica20144442@gmail.com>
2023-06-14 12:49:27 +0900

    [Platinum IV] Title: 공약수열, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Platinum/14252. 공약수열/공약수열.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = sorted(list(map(int, input().split())))
+count = 0
+arr2 = []
+
+def gcd(a, b):
+    while a % b != 0:
+        tmp = a % b
+        a = b
+        b = tmp
+    return b
+
+for i in range(len(arr) - 1):
+    if gcd(arr[i], arr[i+1]) > 1:
+        arr2.append([arr[i], arr[i+1]])
+
+for a, b in arr2:
+    for j in range(a+1, b):
+        tmp = 0
+
+        if gcd(a, j) == 1:
+            tmp += 1
+        if gcd(b, j) == 1:
+            tmp += 1
+
+        if tmp > 1:
+            count += 1
+            break
+        if j == b - 1:
+            count += 2
+
+print(count)
