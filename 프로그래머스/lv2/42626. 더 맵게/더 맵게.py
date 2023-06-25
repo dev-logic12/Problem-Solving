1e8a7e36c4562f6632bf449e3bd8c195521c1119
monica Lee <monica20144442@gmail.com>
2023-06-25 11:20:08 +0900

    [level 2] Title: 더 맵게, Time: 2109.77 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -1,16 +1,17 @@
 import heapq
 
 def solution(scoville, k):
-    answer = 0
-    heapq.heapify(scoville)  # scoville 리스트를 힙으로 변환
-    while len(scoville) > 1:  # 음식이 2개 이상 남아있는 동안 반복
-        min1 = heapq.heappop(scoville)  # 가장 작은 원소를 pop
-        if min1 >= k:  # 가장 작은 원소가 k보다 크거나 같으면 반복 종료
+    answer = 0 
+    heapq.heapify(scoville)
+    while len(scoville) > 1:
+        min1 = heapq.heappop(scoville)
+        if min1 >= k:
             break
-        min2 = heapq.heappop(scoville)  # 두 번째로 작은 원소를 pop
-        heapq.heappush(scoville, min1 + (min2 * 2))  # 두 개의 원소를 섞어서 다시 push
-        answer += 1  # 섞은 횟수를 카운트
-    else:  # 반복이 정상적으로 종료된 경우 (k보다 크거나 같은 원소가 없는 경우)
-        if scoville[0] < k:  # 남은 가장 작은 원소가 k보다 작으면 -1 반환
+        min2 = heapq.heappop(scoville)
+        heapq.heappush(scoville, min1+ (min2*2))
+        answer += 1 
+    else:
+        if scoville[0] < k:
             return -1
-    return answer
+    return answer 
+
