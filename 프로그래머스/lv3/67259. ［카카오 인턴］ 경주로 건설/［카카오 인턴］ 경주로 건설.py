9d1daa0344a76b431b06e049fcea33c35ac8a1a1
monica Lee <monica20144442@gmail.com>
2023-06-27 20:39:23 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,40 +1,21 @@
 from collections import deque
+import math
 
-def calculate_cost(current_direction, next_direction, current_cost):
-    # 현재 방향과 다음 방향이 동일한 경우 또는 상하 혹은 좌우 방향인 경우 100을 추가한 비용 반환
-    if (current_direction in ('R', 'L') and next_direction in ('L', 'R')) or (current_direction in ('D', 'U') and next_direction in ('D', 'U')):
-        return current_cost + 100
-    # 그 외의 경우 600을 추가한 비용 반환
-    return current_cost + 600
-
-def bfs(start_x, start_y, initial_cost, initial_direction):
-    queue = deque([(start_x, start_y, initial_cost, initial_direction)])
-    checked = [[0 for _ in range(N)] for _ in range(N)]
-    checked[start_x][start_y] = 1
-    while queue:
-        x, y, cost, current_direction = queue.popleft()
-        # 목적지에 도착한 경우 최소 비용을 answer에 추가하고 다음으로 넘어감
-        if x == N-1 and y == N-1:
-            answer.append(cost)
-            continue
-        # 상하좌우 방향 탐색
-        for i, j, d in [(0, 1, 'R'), (1, 0, 'D'), (0, -1, 'L'), (-1, 0, 'U')]:
-            new_x, new_y, new_cost = x+i, y+j, calculate_cost(current_direction, d, cost)
-            # 새로운 좌표가 범위를 벗어나거나 벽인 경우 무시
-            if new_x < 0 or new_y < 0 or new_x >= N or new_y >= N or new_board[new_x][new_y]:
-                continue
-            # 새로운 좌표의 방문 여부를 확인하고, 비용이 더 작은 경우에만 큐에 추가
-            if not checked[new_x][new_y] or checked[new_x][new_y] > new_cost:
-                checked[new_x][new_y] = new_cost
-                queue.append((new_x, new_y, new_cost, d))
+dx,dy = [0,0,1,-1],[1,-1,0,0]
 
 def solution(board):
-    global N, checked, new_board, answer
-    answer = []
-    N = len(board)
-    new_board = [board[i][:] for i in range(N)]
-    # 오른쪽 방향과 아래쪽 방향에서 시작하는 BFS 탐색
-    bfs(0, 0, 0, 'R')
-    bfs(0, 0, 0, 'D')
-    # answer 리스트에서 최소 비용 반환
-    return min(answer)
+    def bfs(startX,startY,startC,startD):
+        n = len(board)
+        result = [[math.inf for _ in range(n)] for _ in range(n)]
+        q = deque()
+        q.append((startX,startY,startC,startD))
+        result[startX][startY] = 0
+        while q:
+            x,y,cost,direction = q.popleft()
+            for i in range(4):
+                nx,ny,ncost = x + dx[i], y + dy[i], cost+600 if i != direction else cost+100
+                if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 0 and result[nx][ny] > ncost:
+                    result[nx][ny] = ncost
+                    q.append((nx,ny,ncost,i))
+        return result[-1][-1]
+    return min(bfs(0,0,0,0),bfs(0,0,0,2))
