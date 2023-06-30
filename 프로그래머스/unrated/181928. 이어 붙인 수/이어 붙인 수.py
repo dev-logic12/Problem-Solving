9958e7fa9f9c97172559d92c6de3e7335431aaba
monica Lee <monica20144442@gmail.com>
2023-07-01 07:50:58 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
@@ -1,11 +1,9 @@
 def solution(num_list):
-    even = ""
-    odd = ""
+    even = ''
+    odd = ''
     for num in num_list:
-        if num % 2 == 0:
+        if num%2 == 0:
             even += str(num)
         else:
             odd += str(num)
-    even_sum = int(even) if even else 0
-    odd_sum = int(odd) if odd else 0
-    return int(odd) + int(even)
+    return int(even) + int(odd)
