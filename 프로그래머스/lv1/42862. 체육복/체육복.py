5570466871d66c76920ca6a9b3a58407d4ae2798
monica Lee <monica20144442@gmail.com>
2023-07-15 14:25:27 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -13,8 +13,8 @@ def solution(n, lost, reserve):
                 answer[i] -= 1 
             elif answer[i+1] == 0:
                 answer[i+1] += 1 
-                answer[i] -= 1
+                answer[i] -= 1 
     for i in range(1, n+1):
         if answer[i] >= 1:
             cnt += 1 
-    return cnt 
+    return cnt
