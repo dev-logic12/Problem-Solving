0edbb3bc50046c0565c53a65fa71d11c8d76258c
monica Lee <monica20144442@gmail.com>
2023-07-19 14:27:14 +0900

    [unrated] Title: 정사각형으로 만들기, Time: 0.00 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.py
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.py
@@ -1,19 +1,17 @@
 def solution(arr):
-    row_len = len(arr)
-    col_len = len(arr[0])
+    row = len(arr)
+    col = len(arr[0])
 
-    # 행의 수가 더 많은 경우
-    if row_len > col_len:
-        diff = row_len - col_len
-        for i in range(row_len):
+    if row > col:
+        diff = row - col
+        for i in range(row):
             for _ in range(diff):
                 arr[i].append(0)
 
-    # 열의 수가 더 많은 경우
-    elif col_len > row_len:
-        diff = col_len - row_len
+    elif col > row:
+        diff = col - row
         for _ in range(diff):
-            arr.append([0] * col_len)
+            arr.append([0] * col)
 
     return arr
 
