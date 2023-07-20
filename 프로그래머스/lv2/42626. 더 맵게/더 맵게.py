ce4dc0d6e7650cc3d46d4605acd0e0f09fbe586f
monica Lee <monica20144442@gmail.com>
2023-07-20 22:31:29 +0900

    [level 2] Title: 더 맵게, Time: 1967.79 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -1,17 +1,15 @@
 import heapq
 
 def solution(scoville, k):
-    answer = 0 
-    heapq.heapify(scoville)
-    while len(scoville) > 1:
-        min1 = heapq.heappop(scoville)
-        if min1 >= k:
-            break
-        min2 = heapq.heappop(scoville)
-        heapq.heappush(scoville, min1+ (min2*2))
-        answer += 1 
-    else:
-        if scoville[0] < k:
+    heapq.heapify(scoville) 
+    answer = 0
+    
+    while scoville[0] < k:
+        if len(scoville) <= 1:
             return -1
-    return answer 
-
+        min1 = heapq.heappop(scoville)
+        min2 = heapq.heappop(scoville)
+        heapq.heappush(scoville, min1 + (min2 * 2))
+        answer += 1 
+    
+    return answer
