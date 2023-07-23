d440b0513b1501057a0f773403bd18cfeb633662
monica Lee <monica20144442@gmail.com>
2023-07-23 11:50:45 +0900

    [level 2] Title: 타겟 넘버, Time: 3.46 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,10 +1,10 @@
-def recursive(numbers, sum, target):
+def recur(numbers, sum, target):
     if numbers == []:
         if sum == target:
             return 1
         else:
             return 0
-    return recursive(numbers[1:], sum + numbers[0], target) + recursive(numbers[1:], sum - numbers[0], target)
+    return recur(numbers[1:], sum + numbers[0], target) + recur(numbers[1:], sum - numbers[0], target)
 
 def solution(numbers, target):
-    return recursive(numbers, 0, target)
+    return recur(numbers, 0, target)
