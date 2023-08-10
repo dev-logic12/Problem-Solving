a14c5c297b84e234144e29d991849abd9744fb6b
monica Lee <monica20144442@gmail.com>
2023-08-10 18:40:03 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,20 +1,20 @@
 def solution(n, lost, reserve):
-    member = [1]*(n+2)
+    answer = [1]*(n+2)
     cnt = 0 
     for i in range(1, n+1):
         if i in lost:
-            member[i] -= 1 
+            answer[i] -= 1
         if i in reserve:
-            member[i] += 1 
+            answer[i] += 1 
     for i in range(1, n+1):
-        if member[i] == 2:
-            if member[i-1] == 0:
-                member[i-1] += 1 
-                member[i] -= 1 
-            elif member[i+1] == 0:
-                member[i+1] += 1 
-                member[i] -= 1 
+        if answer[i] == 2:
+            if answer[i-1] == 0:
+                answer[i] -= 1 
+                answer[i-1] += 1
+            elif answer[i+1] == 0:
+                answer[i] -= 1 
+                answer[i+1] += 1 
     for i in range(1, n+1):
-        if member[i] >= 1:
+        if answer[i] >= 1:
             cnt += 1 
     return cnt
