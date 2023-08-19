996cf2d1f74ba12dd8e5a66ed9b0c1cd1be65144
monica Lee <monica20144442@gmail.com>
2023-08-19 10:50:55 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -4,15 +4,3 @@ def solution(array, commands):
         answer = array[i-1:j]
         result.append(sorted(answer)[k-1])
     return result
-
-
-
-
-
-# def solution(array, commands):
-#     answer = []
-#     for command in commands:
-#         i, j, k = command
-#         answer1 = array[i-1:j]
-#         answer.append(sorted(answer1)[k-1])
-#     return answer
