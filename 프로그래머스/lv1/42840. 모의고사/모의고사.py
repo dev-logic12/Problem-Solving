e8666fb44dd202f79e55ab9df183219b808925f1
monica Lee <monica20144442@gmail.com>
2023-08-21 08:41:11 +0900

    [level 1] Title: 모의고사, Time: 5.20 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,22 +1,11 @@
 def solution(answers):
-    answer = []
-    s1 = [1,2,3,4,5] * 2000
-    s2 = [2,1,2,3,2,4,2,5] * 1250
-    s3 = [3,3,1,1,2,2,4,4,5,5] * 1000
-    p1, p2, p3 = 0, 0, 0
+    p = [[1, 2, 3, 4, 5],
+         [2, 1, 2, 3, 2, 4, 2, 5],
+         [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    s = [0] * len(p)
 
-    for i in range(len(answers)):
-        if s1[i] == answers[i]:
-            p1 += 1 
-        if s2[i] == answers[i]:
-            p2 += 1 
-        if s3[i] == answers[i]:
-            p3 += 1 
-    count = max(p1, p2, p3)
-    if count == p1:
-        answer.append(1)
-    if count == p2:
-        answer.append(2)
-    if count == p3:
-        answer.append(3)
-    return answer
+    for q, a in enumerate(answers):
+        for i, v in enumerate(p):
+            if a == v[q % len(v)]:
+                s[i] += 1
+    return [i + 1 for i, v in enumerate(s) if v == max(s)]
