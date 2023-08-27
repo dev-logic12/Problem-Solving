5e62e4e13f1feed02f2ae8bb17fe36e8d9b4d247
monica Lee <monica20144442@gmail.com>
2023-08-28 07:15:20 +0900

    [level 2] Title: 구명보트, Time: 10.39 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,12 +1,15 @@
+from collections import deque
+
 def solution(people, limit):
-    people.sort()
-    left, right = 0, len(people) - 1
-    boats = 0
+    result = 0
+    deque_people = deque(sorted(people))
 
-    while left <= right:
-        if people[left] + people[right] <= limit:
-            left += 1
-        right -= 1
-        boats += 1
-
-    return boats
+    while deque_people:
+        left = deque_people.popleft()
+        if not deque_people:
+            return result + 1
+        right = deque_people.pop()
+        if left + right > limit:
+            deque_people.appendleft(left)
+        result += 1
+    return result
