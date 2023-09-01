de2e61de3d754d74cac9f87c362343d771cf7341
monica Lee <monica20144442@gmail.com>
2023-09-01 23:02:00 +0900

    [level 2] Title: 올바른 괄호, Time: 6.97 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,7 +1,10 @@
 def solution(s):
     answer = 0
     for c in s :
-        if c == '(' : answer += 1
-        elif c == ')' : answer -= 1
-        if answer < 0 : return False
+        if c == ')' : 
+            answer -= 1
+        elif c == '(' : 
+            answer += 1
+        if answer < 0 : 
+            return False
     return answer == 0
