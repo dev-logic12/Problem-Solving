5f15a63badeaf8d623b9e33f16e7d0341e02dc05
monica Lee <monica20144442@gmail.com>
2023-09-04 21:54:00 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,11 +1,8 @@
 def solution(brown, yellow):
-    answer = []
-    total = brown + yellow                  # a * b = total
-    for b in range(1,total+1):
-        if (total / b) % 1 == 0:            # total / b = a
-            a = total / b
-            if a >= b:                      # a >= b
-                if 2*a + 2*b == brown + 4:  # 2*a + 2*b = brown + 4 
-                    return [a,b]
-            
-    return answer
+    w = (brown / 2) + 1
+    h = 1
+    while w >= h:
+        if (w - 2) * (h - 2) == yellow:
+            return [w, h]
+        w -= 1
+        h += 1
