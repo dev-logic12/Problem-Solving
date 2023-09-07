3fb124e21472d857093aaaf9b9494e0ebac98c4e
monica Lee <monica20144442@gmail.com>
2023-09-07 23:24:18 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,9 +1,8 @@
 def solution(n, costs):
     answer = 0
-    costs.sort(key = lambda x: x[2]) # 비용기준으로 오름차순 정렬
-    connect = set([costs[0][0]]) # 연결을 확인하는 집합
+    costs.sort(key = lambda x: x[2]) 
+    connect = set([costs[0][0]])
     
-    # Kruskal 알고리즘으로 최소 비용 구하기
     while len(connect) != n:
         for cost in costs:
             if cost[0] in connect and cost[1] in connect:
