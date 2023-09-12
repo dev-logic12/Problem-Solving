a92bdb0f7547e987bc91215a3f73545c01cbc5fb
monica Lee <monica20144442@gmail.com>
2023-09-12 22:50:29 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,12 +1,35 @@
 def solution(n, lost, reserve):
-    reserved = 0
-    lostN = list(set(lost) - set(reserve))
-    reserveN = list(set(reserve) - set(lost))
-    lostN.sort()
-    for l in lostN:
-        for x in range(l-1, l+2):
-            if x in reserveN:
-                reserveN.remove(x)
-                reserved += 1
-                break
-    return n - len(lostN) + reserved
+    answer = [1]*(n+2)
+    cnt = 0 
+
+    for i in range(1, n+1):
+        if i in lost and i in reserve:
+            lost.remove(i)
+            reserve.remove(i)
+
+    for i in range(1, n+1):
+        if i in lost:
+            if answer[i] == 0:
+                continue
+            answer[i] -= 1 
+        if i in reserve:
+            if answer[i] == 2:
+                continue
+            answer[i] += 1 
+    
+    
+    for i in range(1, n+1):
+        if answer[i] == 2:
+            if answer[i-1] == 0:
+                answer[i-1] += 1 
+                answer[i] -= 1 
+            elif answer[i+1] == 0:
+                answer[i+1] += 1
+                answer[i] -= 1 
+    
+
+    for i in range(1, n+1):
+        if answer[i] >= 1:
+            cnt += 1 
+    return cnt
+
