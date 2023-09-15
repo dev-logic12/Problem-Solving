a8080f16500f63ba61605777c2087303623b9b29
monica Lee <monica20144442@gmail.com>
2023-09-15 19:55:49 +0900

    [unrated] Title: 커피 심부름, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
@@ -1,12 +1,12 @@
 def solution(order):
     answer = 0
-    a = ['iceamericano', 'americanoice', 'hotamericano', 'americanohot', 'americano', 'anything']
-    l = ['icecafelatte', 'cafelatteice', 'hotcafelatte', 'cafelattehot', 'cafelatte']
+    cheap = ['iceamericano', 'americanoice', 'hotamericano', 'americanohot', 'americano', 'anything']
+    expensive = ['icecafelatte', 'cafelatteice', 'hotcafelatte', 'cafelattehot', 'cafelatte']
     
     for menu in order:
-        if menu in a:
+        if menu in cheap:
             answer += 4500
-        elif menu in l:
+        elif menu in expensive:
             answer += 5000
     
     return answer
