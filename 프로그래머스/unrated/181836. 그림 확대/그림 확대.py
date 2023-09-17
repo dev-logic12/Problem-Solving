d69cf30f691153582560306755259b1291c43984
monica Lee <monica20144442@gmail.com>
2023-09-17 12:39:14 +0900

    [unrated] Title: 그림 확대, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181836. 그림 확대/그림 확대.py
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.py
@@ -1,13 +1,6 @@
 def solution(picture, k):
-    row_len = len(picture)
-    col_len = len(picture[0])
-
-    new_picture = []
-
-    for row in picture:
-        new_row = ''
-        for pixel in row:
-            new_row += pixel * k
-        new_picture.extend([new_row] * k)
-
-    return new_picture
+    answer = []
+    for i in range(len(picture)):
+        for _ in range(k):
+            answer.append(picture[i].replace('.', '.' * k).replace('x', 'x' * k))
+    return answer
