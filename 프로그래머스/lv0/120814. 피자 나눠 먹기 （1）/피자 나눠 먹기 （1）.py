9d20a7236ec9d9d4454de8fee2f54b9616fdf791
monica Lee <monica20144442@gmail.com>
2023-10-06 22:34:10 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -1,10 +1,7 @@
 def solution(n):
-  # 7조각 7사람 1판 
-  # 7조각 1사람 1판 
-  # 7조각 15사람 3판 
-  # n//7 == 0이면 몫을 반환 
-  # n//7 아니면 몫+1을 반환 
-  if n%7 == 0:
-    return n//7 
-  else:
-    return (n//7)+1   
+    answer = 0
+    if n % 7 == 0:
+        answer = n // 7
+    else:
+        answer = (n // 7) + 1
+    return answer
