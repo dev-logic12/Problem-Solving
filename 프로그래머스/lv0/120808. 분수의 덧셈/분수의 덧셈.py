35584c860b45a55bac9e94a310c45d6fb2388ae1
monica Lee <monica20144442@gmail.com>
2023-10-08 14:30:53 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,7 @@
+
+
+from fractions import Fraction
+
+def solution(denum1, num1, denum2, num2):
+    answer = Fraction(denum1, num1) + Fraction(denum2, num2)
+    return [answer.numerator, answer.denominator]
