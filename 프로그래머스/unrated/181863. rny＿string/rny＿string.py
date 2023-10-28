1ee675110e190749ce4ab2c53659787a27f633cc
monica Lee <monica20144442@gmail.com>
2023-10-28 21:31:47 +0900

    [level unrated] Title: rny_string, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.py
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.py
@@ -1,9 +1,2 @@
 def solution(rny_string):
-    answer = ''
-    for i in rny_string:
-        answer = rny_string.replace('m', 'rn')    
-    return answer
-
-
-# rny_string	= "masterpiece"	
-# print(solution(rny_string))
+    return rny_string.replace('m', 'rn')
