0729cc00cda3aeb50b6d019cf4ba7ee98d91ff89
monica Lee <monica20144442@gmail.com>
2023-10-29 08:55:12 +0900

    [level unrated] Title: x 사이의 개수, Time: 1.94 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.py
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.py
@@ -1,2 +1,7 @@
 def solution(myString):
-    return list(map(len, myString.split('x')))
+    answer = []
+    answer1 = []
+    answer = myString.split('x')
+    for i in answer:
+        answer1.append(len(i))
+    return answer1
