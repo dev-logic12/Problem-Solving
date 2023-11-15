5a2ea9f888bd36b0504e230734f2e2dcdeb8af43
monica Lee <monica20144442@gmail.com>
2023-11-15 22:27:02 +0900

    [level unrated] Title: A 강조하기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.py
@@ -1,12 +1,2 @@
 def solution(myString):
-    answer = ''
-    for i in myString:
-        if i == 'a':
-            answer += i.replace('a', 'A')
-        elif i == 'A':
-            answer += i 
-        elif i.isupper() == True:
-            answer += i.lower()
-        else:
-            answer += i 
-    return answer
+    return myString.lower().replace('a','A') 
