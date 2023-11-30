1a308540e0487a1086c36cb746698e52c9484b8d
monica Lee <monica20144442@gmail.com>
2023-11-30 22:25:11 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,7 +1,7 @@
 def solution(num_list):
     answer = 1
     answer1 = 0 
-    for num in num_list:
-        answer1 += num
-        answer *= num
-    return 1 if answer <= answer1*answer1 else 0
+    for i in num_list:
+        answer *= i
+        answer1 += i 
+    return 1 if answer< answer1*answer1 else 0  
