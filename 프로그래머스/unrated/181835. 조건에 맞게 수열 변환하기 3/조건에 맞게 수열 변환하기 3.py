693d719fb5f8d74d34535b0f862db43e53617112
monica Lee <monica20144442@gmail.com>
2023-12-30 21:04:09 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 6.46 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
@@ -1,9 +1,2 @@
 def solution(arr, k):
-    answer = []
-    for i in arr:
-        if k%2 != 0:
-            answer.append(i*k)
-        else:
-            answer.append(i+k)
-            
-    return answer
+    return [i*k if k%2!=0 else i+k for i in arr]
