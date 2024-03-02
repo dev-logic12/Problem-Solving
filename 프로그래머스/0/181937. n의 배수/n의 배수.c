dd8de9c7fb1e7353594e76bf519027290e103315
monica Lee <monica20144442@gmail.com>
2024-03-02 20:22:05 +0900

    [level 0] Title: n의 배수, Time: 0.01 ms, Memory: 4.53 MB -BaekjoonHub

--- 프로그래머스/0/181937. n의 배수/n의 배수.c
+++ 프로그래머스/0/181937. n의 배수/n의 배수.c
@@ -1,1 +1,12 @@
-solution(int num, int n){return !(num % n);}
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num, int n) {
+    int answer = 0;
+
+    if(num%n==0) {
+        answer= 1;
+    }
+    return answer;
+}
