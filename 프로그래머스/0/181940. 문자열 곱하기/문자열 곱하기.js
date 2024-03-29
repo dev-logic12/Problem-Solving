ca34dc4c88e75fe101b9c44a3ea42fd49e4bffa2
monica Lee <monica20144442@gmail.com>
2024-03-29 23:21:37 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
@@ -1,4 +1,9 @@
 function solution(my_string, k) {
-    return my_string.repeat(k)
-}
+    var answer = '';
 
+    for(let i = 0; i<k; i++){
+        answer += my_string;
+    }
+
+    return answer;
+}
