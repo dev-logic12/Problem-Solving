16716e5ffdc632edb3a4c57ea05c925391365518
monica Lee <monica20144442@gmail.com>
2024-05-22 23:23:29 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.py
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.py
@@ -1,6 +1,7 @@
 def solution(numbers, n):
     answer = 0
-    for i in numbers:
-        answer += i
-        if answer > n:
-            return answer
+    i=0
+    while answer<=n:
+        answer+=numbers[i]
+        i+=1
+    return answer
