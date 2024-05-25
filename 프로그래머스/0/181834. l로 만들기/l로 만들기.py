3e9fae1cd1e417914fe2122bdf7c1a1fc50ff215
monica Lee <monica20144442@gmail.com>
2024-05-25 23:35:00 +0900

    [level 0] Title: l로 만들기, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -1,3 +1,2 @@
 def solution(myString):
-    answer = [x if x > 'l' else 'l' for x in myString]
-    return ''.join(answer)
+    return myString.translate(str.maketrans('abcdefghijk', 'lllllllllll'))
