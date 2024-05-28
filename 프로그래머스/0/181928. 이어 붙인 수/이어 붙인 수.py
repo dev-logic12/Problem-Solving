c9e365a2ee049e1767bea60a9b5c15784f5498f5
monica Lee <monica20144442@gmail.com>
2024-05-28 23:53:02 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -1,9 +1,10 @@
 def solution(num_list):
-    h = []
-    z = []
+    answer = 0
+    hol=""
+    zzak=""
     for i in num_list:
-        if i % 2 != 0:
-            h.append(str(i))
+        if i%2!=0:
+            hol+=str(i)
         else:
-            z.append(str(i))
-    return int(''.join(h))+int(''.join(z))
+            zzak+=str(i)
+    return int(hol)+int(zzak)
