4be60f1d7dc7dd60f22b4f32ef73d275b348bee2
monica Lee <monica20144442@gmail.com>
2024-06-24 23:21:48 +0900

    [level 0] Title: 부분 문자열, Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
@@ -1,5 +1,8 @@
 class Solution {
     public int solution(String str1, String str2) {
-        return str2.contains(str1) ? 1 : 0;
+        int answer = 0;
+        answer = str2.contains(str1) ? 1 : 0;
+        return answer;
     }
 }
+
