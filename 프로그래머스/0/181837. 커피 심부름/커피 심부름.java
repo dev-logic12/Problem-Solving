be0c70dec828e70be8523ebb2240a7a1874e25f2
monica Lee <monica20144442@gmail.com>
2024-07-19 23:17:40 +0900

    [level 0] Title: 커피 심부름, Time: 1.19 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -1,12 +1,8 @@
 class Solution {
     public int solution(String[] order) {
         int answer = 0;
-        for(String i : order) {
-            if(i.contains("cafelatte")) {
-                answer += 5000;
-            } else {
-                answer += 4500;
-            }
+        for(String menu : order) {
+            answer += menu.contains("cafelatte")?5000:4500;
         }
         return answer;
     }
