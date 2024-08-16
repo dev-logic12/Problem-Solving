55b7af0a3be0b4867fd25dd427c61ee037340554
monica Lee <monica20144442@gmail.com>
2024-08-16 23:28:50 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.27 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -1,8 +1,2 @@
-import numpy as np
-
 def solution(arr):
-    matrix = np.array(arr)
-    if np.array_equal(matrix, matrix.T):
-        return 1
-    else:
-        return 0
+    return int(arr == list(map(list, zip(*arr))))
