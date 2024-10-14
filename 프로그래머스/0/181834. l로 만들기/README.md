8280d939c138fdc46704581f29ed12a1ffc17577
monica Lee <monica20144442@gmail.com>
2024-10-14 23:31:48 +0900

    [level 0] Title: l로 만들기, Time: 0.11 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/README.md
+++ 프로그래머스/0/181834. l로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] l로 만들기 - 181834 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181834?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181834?language=c) 
 
 ### 성능 요약
 
-메모리: 36.6 MB, 시간: 5.94 ms
+메모리: 4.22 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 30일 23:09:47
+2024년 10월 14일 23:31:44
 
 ### 문제 설명
 
--- 프로그래머스/0/181834. l로 만들기/l로 만들기.c
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.c
@@ -4,8 +4,10 @@
 
 char* solution(const char* myString) {
     int len = strlen(myString);
-    char* answer = (char*)malloc(len + 1);
-    for(int i = 0; i < len; i++) {
+    char* answer = malloc(len + 1);
+    if (!answer) return NULL;
+    
+    for (int i = 0; i < len; i++) {
         answer[i] = myString[i] < 'l' ? 'l' : myString[i];
     }
     answer[len] = '\0';
@@ -15,7 +17,13 @@ char* solution(const char* myString) {
 int main() {
     char myString[] = "abcdefghijklm";
     char* result = solution(myString);
-    printf("Modified string: %s\n", result);
-    free(result);
+    
+    if (result) {
+        printf("Modified string: %s\n", result);
+        free(result);
+    } else {
+        printf("Memory allocation failed\n");
+    }
+
     return 0;
 }
