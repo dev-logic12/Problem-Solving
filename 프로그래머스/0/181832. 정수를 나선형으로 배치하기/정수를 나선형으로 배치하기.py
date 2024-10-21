c4a1a7f92afcf52e0a40621ddb88f6acaca824cf
monica Lee <monica20144442@gmail.com>
2024-10-21 23:14:45 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.50 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,25 +1,11 @@
 def solution(n):
-    # 초기화: n x n 크기의 배열을 0으로 초기화
-    answer = [[0] * n for _ in range(n)]
-    
-    # 방향 벡터: 우(0,1), 하(1,0), 좌(0,-1), 상(-1,0)
-    move = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-    
-    x, y, m = 0, 0, 0  # 시작 위치와 방향 초기화
+    answer = [[None] * n for _ in range(n)]
+    move = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    x, y, m = 0, 0, 0
     for i in range(1, n**2 + 1):
-        answer[y][x] = i  # 현재 위치에 값을 넣기
-        # 다음 위치 계산
+        answer[y][x] = i
         next_y, next_x = y + move[m][0], x + move[m][1]
-        
-        # 다음 위치가 경계 바깥이거나 이미 숫자가 채워져 있는 경우 방향 전환
-        if not (0 <= next_y < n and 0 <= next_x < n and answer[next_y][next_x] == 0):
-            m = (m + 1) % 4  # 방향 전환
-            
-        # 새로운 위치로 이동
+        if not (0 <= next_y < n and 0 <= next_x < n) or answer[next_y][next_x] is not None:
+            m = (m + 1) % 4
         y, x = y + move[m][0], x + move[m][1]
-    
     return answer
-
-# 테스트 예제
-print(solution(3))  # [[1, 2, 3], [8, 9, 4], [7, 6, 5]]
-print(solution(4))  # [[1, 2, 3, 4], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7]]
