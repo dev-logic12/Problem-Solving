e1902e98be355a7daf6872350414415318fcf8ff
monica Lee <monica20144442@gmail.com>
2024-11-09 23:44:58 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.59 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/README.md
+++ 프로그래머스/0/181830. 정사각형으로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 정사각형으로 만들기 - 181830 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181830?language=ruby) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181830?language=javascript) 
 
 ### 성능 요약
 
-메모리: 14.2 MB, 시간: 0.50 ms
+메모리: 38.3 MB, 시간: 0.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 09일 23:48:49
+2024년 11월 09일 23:44:53
 
 ### 문제 설명
 
--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -1,34 +1,7 @@
+
 function solution(arr) {
-  var answer = [];
-  
-  // arr의 행(row)과 열(column) 수 계산
-  const rowCount = arr.length;
-  const colCount = arr[0].length;
-  
-  // 행(row)과 열(column)의 길이 차이에 따라 처리
-  if (rowCount > colCount) {
-    // 행이 더 많을 때: 열을 늘려 0으로 채움
-    for (let i = 0; i < rowCount; i += 1) {
-      const newArr = [...arr[i]];
-      for (let j = colCount; j < rowCount; j += 1) {
-        newArr.push(0); // 열을 행의 수만큼 0으로 채움
-      }
-      answer.push(newArr);
-    }
-  } else if (rowCount < colCount) {
-    // 열이 더 많을 때: 부족한 행을 0으로 채운 배열로 추가
-    for (let i = 0; i < colCount; i += 1) {
-      if (i < rowCount) {
-        answer.push(arr[i]);
-      } else {
-        const newArr = Array(colCount).fill(0); // 새로운 행을 0으로 채움
-        answer.push(newArr);
-      }
-    }
-  } else {
-    // 행과 열의 길이가 같을 때: 그대로 추가
-    answer = arr;
-  }
-  
-  return answer;
+    let n = Math.max(arr.length, Math.max(...arr.map(v=>v.length)));
+    for (let a of arr) while (a.length<n) a.push(0);
+    while (arr.length < n) arr.push(Array(n).fill(0));
+    return arr;
 }
