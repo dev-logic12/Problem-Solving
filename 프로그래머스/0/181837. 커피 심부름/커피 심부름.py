72f8980a04fb3b16adecdf204f84979fa30d35ae
monica Lee <monica20144442@gmail.com>
2024-11-30 23:14:21 +0900

    [level 0] Title: 커피 심부름, Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -1,7 +1,2 @@
 def solution(order):
-    answer = 0
-    for want in order:
-        if 'latte' in want:
-            answer += 500
-        answer += 4500
-    return answer
+    return sum([5000 if "cafelatte" in i else 4500 for i in order])
