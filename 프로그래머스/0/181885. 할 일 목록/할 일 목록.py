b445e887fae01c1125bf3f16271b0fa963be787e
monica Lee <monica20144442@gmail.com>
2024-12-23 23:15:52 +0900

    [level 0] Title: 할 일 목록, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,6 +1,6 @@
 def solution(todo_list, finished):
-    todo = []
+    do = []
     for i in range(len(finished)):
         if finished[i] == False:
-            todo.append(todo_list[i])
-    return todo
+            do.append(todo_list[i])
+    return do
