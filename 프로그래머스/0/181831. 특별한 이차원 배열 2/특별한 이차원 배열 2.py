7e2a0d05b67e086e5fb5c4adc44f239ca11f6d6b
monica Lee <monica20144442@gmail.com>
2024-12-31 17:33:28 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 1.14 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -1,3 +1,6 @@
 def solution(arr):
-    answer = int(arr == list(map(list, zip(*arr))))
-    return answer 
+    for i in range(1, len(arr)):
+        for j in range(len(arr)):
+            if arr[i][j] != arr[j][i]:
+                return 0
+    return 1
