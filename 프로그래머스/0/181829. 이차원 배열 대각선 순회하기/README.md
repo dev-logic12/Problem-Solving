9484f7818299d859d62c389be2750c4b0771be84
monica Lee <monica20144442@gmail.com>
2025-01-10 21:54:07 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 0.37 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/README.md
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 이차원 배열 대각선 순회하기 - 181829 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181829?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181829) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.36 ms
+메모리: 10.3 MB, 시간: 0.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 30일 23:35:35
+2025년 01월 10일 21:54:02
 
 ### 문제 설명
 
--- 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.py
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.py
@@ -1,13 +1,9 @@
 def solution(board, k):
+    answer = 0
 
-    total_sum = 0  # 결과 값을 저장할 변수
-    rows, cols = len(board), len(board[0])  # 행(row)과 열(column)의 크기
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if i + j <= k:
+                answer += board[i][j]
 
-    # 각 행을 순회
-    for i in range(rows):
-        # 현재 행에서 열의 범위를 결정 (k - i와 cols - 1 중 더 작은 값까지 탐색)
-        max_col = min(k - i, cols - 1)
-        for j in range(max_col + 1):
-            total_sum += board[i][j]  # 해당 값을 누적
-
-    return total_sum
+    return answer
