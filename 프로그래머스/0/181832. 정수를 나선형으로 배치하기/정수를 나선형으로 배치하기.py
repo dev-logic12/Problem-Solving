2d847c1d400c70fee635a95f5897bb2e63a03515
monica Lee <monica20144442@gmail.com>
2025-01-15 22:45:49 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.53 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,20 +1,13 @@
 def solution(n):
-    spiral = [[0] * n for _ in range(n)]  # 초기화
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # 오른쪽, 아래, 왼쪽, 위
-    row, col = 0, 0  # 시작 위치
-    direction_index = 0  # 방향 인덱스
     
-    for num in range(1, n**2 + 1):
-        spiral[row][col] = num
-        next_row = row + directions[direction_index][0]
-        next_col = col + directions[direction_index][1]
-        
-        # 다음 위치가 경계를 벗어나거나 이미 채워져 있는 경우 방향 전환
-        if not (0 <= next_row < n and 0 <= next_col < n and spiral[next_row][next_col] == 0):
-            direction_index = (direction_index + 1) % 4  # 방향 전환
-            next_row = row + directions[direction_index][0]
-            next_col = col + directions[direction_index][1]
-        
-        row, col = next_row, next_col
+    answer = [[None for j in range(n)] for i in range(n)]
+    move = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    x, y, m = 0, 0, 0
     
-    return spiral
+    for i in range(1, n**2 + 1):
+        answer[y][x] = i
+        if y + move[m][0] >= n or x + move[m][1] >= n or answer[y + move[m][0]][x + move[m][1]]:
+            m = (m + 1) % len(move)
+        y, x = y + move[m][0], x + move[m][1]
+        
+    return answer
