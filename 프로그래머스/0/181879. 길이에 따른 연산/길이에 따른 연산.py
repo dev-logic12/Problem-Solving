3b444ed9ba6677afe9d0f9ec50744e11294893a9
monica Lee <monica20144442@gmail.com>
2025-02-09 10:18:18 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -1,7 +1,3 @@
+from math import prod
 def solution(num_list):
-    if len(num_list)>10: 
-        return sum(num_list)
-    a = 1
-    for x in num_list:
-        a *= x
-    return a
+    return sum(num_list) if len(num_list)>=11 else prod(num_list)
