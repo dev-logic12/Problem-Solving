afe88ce366ecaca6bc61cdc15112fee2c955c785
monica Lee <monica20144442@gmail.com>
2025-02-22 17:33:13 +0900

    [level 0] Title: l로 만들기, Time: 5.94 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -1,3 +1,1 @@
-function solution(myString) {
-    return [...myString].map(char => char >= 'a' && char <= 'l' ? 'l' : char).join('');
-}
+const solution = myString => myString.replace(/[a-k]/g,'l')
