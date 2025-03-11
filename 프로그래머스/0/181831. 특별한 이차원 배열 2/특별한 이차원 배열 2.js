c2eb2eca8dc66b58c9e75484b877f487f4ac5ff1
monica Lee <monica20144442@gmail.com>
2025-03-11 20:18:25 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.48 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,11 +1,11 @@
 function solution(arr) {
-    const a = arr.length;
-
-    for(let i=0; i < a; i++) {
-        for(let j=i+1; j < a; j++) {
-            if(arr[i][j] !== arr[j][i]) return 0;
+    let n = arr.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {  
+            if (arr[i][j] !== arr[j][i]) {
+                return 0;
+            }
         }
     }
-
     return 1;
 }
