56499de60a7909b59f7fc7c7295db7556b1d71df
monica Lee <monica20144442@gmail.com>
2025-08-20 22:12:13 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.02 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -1,12 +1,6 @@
 def solution(n):
     answer = [n]
-
     while n != 1:
-        if n % 2 == 0:
-            n = n // 2
-            answer.append(n)
-        elif n % 2 != 0:
-            n = 3 * n + 1
-            answer.append(n)
-
+        n = n // 2 if n % 2 == 0 else 3 * n + 1
+        answer.append(n)
     return answer
