6697c39f97610249c48389bebd08b897d735cb5e
monica Lee <monica20144442@gmail.com>
2025-08-23 23:26:28 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/README.md
+++ 프로그래머스/0/181906. 접두사인지 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 접두사인지 확인하기 - 181906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181906?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181906?language=c) 
 
 ### 성능 요약
 
-메모리: 9.19 MB, 시간: 0.00 ms
+메모리: 4.22 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 15일 23:18:39
+2025년 08월 23일 23:26:22
 
 ### 문제 설명
 
--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.c
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h>
 #include <string.h>
 
 int solution(const char* my_string, const char* is_prefix) {
-    size_t len = strlen(is_prefix);
-    if (strncmp(my_string, is_prefix, len) == 0) return 1;
-    return 0;
+    size_t prefix_len = strlen(is_prefix);
+    size_t str_len = strlen(my_string);
+
+    if (prefix_len > str_len) return 0;
+
+    return strncmp(my_string, is_prefix, prefix_len) == 0 ? 1 : 0;
 }
