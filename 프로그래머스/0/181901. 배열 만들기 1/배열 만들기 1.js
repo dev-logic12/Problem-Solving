27047729016b3dc96258c039bbc2320e2129dad2
monica Lee <monica20144442@gmail.com>
2025-08-27 22:35:49 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.35 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
@@ -1,8 +1,6 @@
 function solution(n, k) {
-    const len = Math.floor(n / k);
-    const answer = new Array(len);
-    for (let i = 0; i < len; i++) {
-        answer[i] = (i + 1) * k;
-    }
-    return answer;
+    return Array.from(
+        { length: Math.floor(n / k) },
+        (_, i) => (i + 1) * k
+    );
 }
