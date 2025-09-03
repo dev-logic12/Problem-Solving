4541892227f268e3e244ce93eeedb494f80882b8
monica Lee <monica20144442@gmail.com>
2025-09-03 19:39:32 +0900

    [level 0] Title: 수 조작하기 1, Time: 10.86 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/README.md
+++ 프로그래머스/0/181926. 수 조작하기 1/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수 조작하기 1 - 181926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181926) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181926?language=javascript) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 22.45 ms
+메모리: 36.7 MB, 시간: 10.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 11일 22:44:56
+2025년 09월 03일 19:39:26
 
 ### 문제 설명
 
--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,11 +1,8 @@
 function solution(n, control) {
-    for(let i = 0 ; i < control.length ; i++){
-        switch(control[i]) {
-            case "w" : n++;break;
-            case "s" : n--;break;
-            case "d" : n+=10;break;
-            case "a" : n-=10;break;
-        }
-    }
-    return n;
+  const move = [1, -1, 10, -10];
+  const directions = ['w', 's', 'd', 'a'];
+
+  return n + [...control].reduce((acc, c) => {
+    return acc + move[directions.indexOf(c)];
+  }, 0);
 }
