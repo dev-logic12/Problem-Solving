ae145a7c746a5a6791bc84b50685ea5562b0ba7b
monica Lee <monica20144442@gmail.com>
2025-09-05 23:51:01 +0900

    [level 0] Title: 접미사 배열, Time: 0.05 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
@@ -1,8 +1,2 @@
 def solution(my_string):
-    answer = []
-
-    for i in range(len(my_string)):
-        answer.append(my_string[i:])
-    answer.sort()
-
-    return answer
+    return sorted(my_string[i:] for i in range(len(my_string)))
