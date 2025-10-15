4dd5c199df8d80767ef6e36e5fc654ac6ee6b7ad
monica Lee <monica20144442@gmail.com>
2025-10-15 21:03:13 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,4 +1,6 @@
-from math import prod
-
 def solution(num_list):
-    return int(prod(num_list) < sum(num_list) ** 2)
+    s = sum(num_list)
+    p = 1
+    for n in num_list:
+        p *= n
+    return int(p < s * s)
