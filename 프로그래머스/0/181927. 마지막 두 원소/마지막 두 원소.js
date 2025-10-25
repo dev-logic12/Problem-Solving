5bc2fdd41eaf9f6f596a1aa88134161ec8b15513
monica Lee <monica20144442@gmail.com>
2025-10-25 23:04:22 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,4 +1,6 @@
 function solution(num_list) {
-    const [a, b] = [...num_list].reverse();
-    return [...num_list, a > b ? (a-b):a*2];
+    const len = num_list.length;
+    const a = num_list[len - 1];  
+    const b = num_list[len - 2];  
+    return [...num_list, a > b ? a - b : a * 2];
 }
